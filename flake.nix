c7e9da206fa7d38f4774d241c0db8621b7e8c73c
Daniel Hwang <daniel@asta.nix>
2025-05-05 16:01:56 -0500

    Initial nixos config

---
+++ flake.nix
@@ -0,0 +1,34 @@
+{
+  description = "A simple NixOS flake";
+
+  inputs = {
+    # NixOS official package source, using the nixos-24.11 branch here
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+
+    # Home Manager
+    home-manager = {
+	  url = "github:nix-community/home-manager";
+	  inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = { self, nixpkgs, home-manager, ... }@inputs: {
+    # Please replace my-nixos with your hostname
+    nixosConfigurations.asta = nixpkgs.lib.nixosSystem {
+      system = "x86_64-linux";
+      modules = [
+        # Import the previous configuration.nix we used,
+        # so the old configuration file still takes effect
+        ./configuration.nix
+
+	# home-manager module
+	home-manager.nixosModules.home-manager {
+	    home-manager.useGlobalPkgs = true;
+	    home-manager.useUserPackages = true;
+
+	    home-manager.users.daniel = import ./home.nix;
+	}
+      ];
+    };
+  };
+}
