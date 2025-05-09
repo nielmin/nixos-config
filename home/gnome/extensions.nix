ae265c824831c355ce60530c71626ce30d43aca3
Daniel Hwang <daniel@asta.nix>
2025-05-09 01:19:20 -0500

    Fix indentation

--- home/gnome/extensions.nix
+++ home/gnome/extensions.nix
@@ -1,7 +1,7 @@
 { pkgs, ... }:
 
 {
-    home.packages = with pkgs; [
-      gnomeExtensions.battery-health-charging
-    ];
+  home.packages = with pkgs; [
+    gnomeExtensions.battery-health-charging
+  ];
 }
