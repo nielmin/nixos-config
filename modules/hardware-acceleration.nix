cdcced70b4440a2892b2c711bac99fc9b49dd564
Daniel Hwang <daniel@asta.nix>
2025-05-13 04:03:31 -0500

    Add generic hwacc to nuc

---
+++ modules/hardware-acceleration.nix
@@ -0,0 +1,16 @@
+{ pkgs, ...}:
+
+{
+ nixpkgs.config.packageOverrides = pkgs: {
+    intel-vaapi-driver = pkgs.intel-vaapi-driver.override { enableHybridCodec = true; };
+  };
+  hardware.graphics = { # hardware.graphics since NixOS 24.11
+    enable = true;
+    extraPackages = with pkgs; [
+      intel-media-driver # LIBVA_DRIVER_NAME=iHD
+      intel-vaapi-driver # LIBVA_DRIVER_NAME=i965 (older but works better for Firefox/Chromium)
+      libvdpau-va-gl
+    ];
+  };
+  environment.sessionVariables = { LIBVA_DRIVER_NAME = "iHD"; }; # Force intel-media-driver
+}
