05604b8623fcd1842c79e8866df230af98b3ecb8
Daniel Hwang <daniel@asta.nix>
2025-05-14 02:36:18 -0500

    Update tmux config w/o plugins

--- home/shell/tmux.nix
+++ home/shell/tmux.nix
@@ -1,5 +1,23 @@
 {
   programs.tmux = {
     enable = true;
+    baseIndex = 1;
+    terminal = "tmux-256color";
+    mouse = true;
+    extraConfig = ''
+      unbind r
+      bind r source-file ~/.config/tmux/tmux.conf \; display "Config reloaded!"
+
+      set -g renumber-windows on
+      set -g renumber-windows on
+
+      # Customize status bar
+      set -g status-style bg=default
+      set -g status-right "%H:%M"
+
+      # Shift Alt vim keys to switch windows
+      bind -n M-H previous-window
+      bind -n M-L next-window
+    '';
   };
 }
