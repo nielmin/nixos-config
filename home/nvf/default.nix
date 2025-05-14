f27b92558470ce1d15ed08ec8c0b49b260b8b95b
Daniel Hwang <daniel@asta.nix>
2025-05-13 22:27:50 -0500

    Enable some mini.nvim modules

--- home/nvf/default.nix
+++ home/nvf/default.nix
@@ -12,6 +12,13 @@
             shiftwidth = 2;
             tabstop = 2;
           };
+          mini = {
+            completion.enable = true;
+            icons.enable = true;
+            statusline.enable = true;
+            diff.enable = true;
+            git.enable = true;
+          };
         };
     };
   };
