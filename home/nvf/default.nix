f02c5eaae6f88d41d1477f935c03a478c6563246
Daniel Hwang <daniel@asta.nix>
2025-05-13 21:36:47 -0500

    Add basic settings to nvf

--- home/nvf/default.nix
+++ home/nvf/default.nix
@@ -3,6 +3,17 @@
 {
   programs.nvf = {
     enable = true;
+
+    settings = {
+        vim = {
+          viAlias = false;
+          vimAlias = true;
+          options = {
+            shiftwidth = 2;
+            tabstop = 2;
+          };
+        };
+    };
   };
 }
 
