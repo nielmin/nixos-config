f311a1ec9d5d53a0bad3c24aaaf0937d7f8f6538
Daniel Hwang <daniel@asta.nix>
2025-05-14 23:52:46 -0500

    Initial bash shellAliases

--- home/shell/bash.nix
+++ home/shell/bash.nix
@@ -6,6 +6,8 @@
       EDITOR = "nvim";
       VISUAL = "nvim";
     };
+    shellAliases = {
+    };
     bashrcExtra = ''
       export PATH="$PATH:$HOME/bin:$HOME/.local/bin:$HOME/go/bin"
     '';
