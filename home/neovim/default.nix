7273d8d6763a806341beac5ec65e48f58a654707
Daniel Hwang <daniel@asta.nix>
2025-05-15 00:27:05 -0500

    Move nvim options to default.nix

--- home/neovim/default.nix
+++ home/neovim/default.nix
@@ -1,5 +1,17 @@
+{ pkgs, ... }:
+
 {
-  imports = [
-    ./nvim.nix
-  ];
+  programs.neovim = {
+    enable = true;
+    defaultEditor = true;
+    viAlias = true;
+    vimAlias = true;
+    extraLuaConfig = ''
+      vim.o.shiftwidth = 2
+      vim.o.softtabstop = 2
+      vim.o.expandtab = true
+      vim.o.relativenumber = true
+    '';
+  };
 }
+
