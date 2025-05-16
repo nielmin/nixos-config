448538720840ebf86e3294eef9f9967a3187ae5f
Daniel Hwang <daniel@asta.nix>
2025-05-15 21:10:24 -0500

    Add 3d printing system package

--- hosts/nuc/default.nix
+++ hosts/nuc/default.nix
@@ -9,6 +9,7 @@
     [ # Include the results of the hardware scan.
     ../../modules/system.nix
     ../../modules/desktop.nix
+    ../../modules/3dprinting.nix
     ../../modules/hardware-acceleration.nix
     ./hardware-configuration.nix
     ];
