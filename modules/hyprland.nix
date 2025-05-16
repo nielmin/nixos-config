6549c90d916d5a3ea6bac1404ae839fe56463a01
Daniel Hwang <daniel@asta.nix>
2025-05-16 17:49:45 -0500

    Make Hyprland toggleable

--- modules/hyprland.nix
+++ modules/hyprland.nix
@@ -1,8 +1,22 @@
-{ pkgs, ...}:
+{ config, lib, pkgs, ...}:
+with lib;
 
-{
-  programs.hyprland = {
-    enable = true;
-    xwayland.enable = true;
+let
+  cfg = config.desktop.hyprland; in {
+
+  options ={
+    desktop.hyprland.enable = lib.mkEnableOption "Enable Hyprland";
+  };
+
+  config = lib.mkIf cfg.enable {
+    programs.hyprland = {
+      enable = true;
+      xwayland.enable = true;
+    };
+
+    environment.systemPackages = with pkgs; [
+      fuzzel 
+      kitty
+    ];
   };
 }
