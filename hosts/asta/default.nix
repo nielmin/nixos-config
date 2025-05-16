8a63998fd03fafb5b9595c3b8a8882cac29c0c7d
Daniel Hwang <daniel@asta.nix>
2025-05-16 07:56:51 -0500

    Disable hwacc module for asta

--- hosts/asta/default.nix
+++ hosts/asta/default.nix
@@ -11,6 +11,8 @@
     ./hardware-configuration.nix
     ];
 
+  hardware-acceleration.enable = false;
+
   # Bootloader.
   boot.loader.systemd-boot.enable = true;
   boot.loader.systemd-boot.configurationLimit = 5;
