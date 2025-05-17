b5390b83d276818f56ca80b37a7ee6399afee9d4
Daniel Hwang <daniel@asta.nix>
2025-05-17 04:50:01 -0500

    Add commands to .bash_profile

--- home/shell/bash.nix
+++ home/shell/bash.nix
@@ -10,6 +10,12 @@
       ls = "ls --color=auto --group-directories-first";
       ll = "ls -Ahl --color=auto --group-directories-first";
     };
+    profileExtra = ''
+    . "$HOME/.nix-profile/etc/profile.d/hm-session-vars.sh"
+    if uwsm check may-start && uwsm select; then
+	exec uwsm start default
+fi
+    '';
     bashrcExtra = ''
       export PATH="$PATH:$HOME/bin:$HOME/.local/bin:$HOME/go/bin"
     '';
