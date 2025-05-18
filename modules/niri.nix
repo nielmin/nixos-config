23b8aff52bbe1ae30a470588d184d3dcf4da46e9
Daniel Hwang <daniel@asta.nix>
2025-05-17 23:06:11 -0500

    Add xdg-desktop-portal-gtk

--- modules/niri.nix
+++ modules/niri.nix
@@ -11,11 +11,12 @@ let
   config = lib.mkIf cfg.enable {
     environment.systemPackages = with pkgs; [
       niri
+      fuzzel
     ];
 
     xdg.portal.enable = true;
     xdg.portal.extraPortals = with pkgs; [
-      xdg-desktop-portal-wlr
+      xdg-desktop-portal-gtk
     ];
   };
 }
