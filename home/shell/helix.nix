e5c16f52800635c4dc21c89fbb7f7befc323c89f
Daniel Hwang <daniel@asta.nix>
2025-05-18 21:41:02 -0500

    Add basic helix settings

--- home/shell/helix.nix
+++ home/shell/helix.nix
@@ -3,5 +3,21 @@
 {
   programs.helix= {
     enable = true;
+
+    settings = {
+      theme = "tokyonight";
+
+      editor = {
+        line-number = "relative";
+        lsp.display-messages = true;
+      };
+
+      keys.normal = {
+        space.space = "file_picker";
+        space.w = ":w";
+        space.q = ":q";
+      };
+
+    };
   };
 }
