3977d3720defe610c708151290d0cd05c4d756ce
Daniel Hwang <daniel@asta.nix>
2025-05-19 02:30:36 -0500

    Add xdg-desktop-portal-gtk for hyprland

--- modules/hyprland.nix
+++ modules/hyprland.nix
@@ -19,5 +19,11 @@ let
       xwayland.enable = true;
       withUWSM = true;
     };
+
+    xdg.portal.enable = true;
+    xdg.portal.extraPortals = with pkgs; [
+      xdg-desktop-portal-gtk
+    ];
+    xdg.portal.config.common.default = "*";
   };
 }
