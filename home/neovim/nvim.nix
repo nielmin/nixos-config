6b03215eff5e96a200d8b4884ce9263352b64fbc
Daniel Hwang <daniel@asta.nix>
2025-05-20 00:26:14 -0500

    Add some neovim plugins

--- home/neovim/nvim.nix
+++ home/neovim/nvim.nix
@@ -6,11 +6,21 @@
     defaultEditor = true;
     viAlias = true;
     vimAlias = true;
-    extraLuaConfig = ''
+    plugins = with pkgs.vimPlugins; [
+        nvim-lspconfig
+	nvim-treesitter.withAllGrammars
+	plenary-nvim
+	gruvbox-material
+	mini-nvim
+    ];
+    extraLuaConfig = /* lua */ ''
       vim.o.shiftwidth = 2
       vim.o.softtabstop = 2
       vim.o.expandtab = true
       vim.o.relativenumber = true
+      vim.o.termguicolors = true
+      vim.cmd('colorscheme gruvbox-material')
+      vim.g.gruvbox_material_background = 'hard'
     '';
   };
 }
