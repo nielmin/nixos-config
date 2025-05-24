23c5af47b6d3bd561557752a72e47e8bb4479594
Daniel Hwang <daniel@asta.nix>
2025-05-24 15:35:55 -0500

    Remove comment from inputs

--- flake.nix
+++ flake.nix
@@ -2,7 +2,6 @@
   description = "A simple NixOS flake";
 
   inputs = {
-    # NixOS official package source, using the nixos-24.11 branch here
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
 
     # Home Manager
