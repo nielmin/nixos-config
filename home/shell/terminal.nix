4eb772d7e28f339a5be977ef0d29d8cd2fc873be
Daniel Hwang <daniel@asta.nix>
2025-05-24 16:29:19 -0500

    Add Kanso Zen theme

--- home/shell/terminal.nix
+++ home/shell/terminal.nix
@@ -9,6 +9,51 @@ in
     enable = true;
     # custom settings
     settings = {
+      colors = {
+        # Kanso Zen Alacritty Colors
+
+        primary = {
+          background = "#090E13";
+          foreground = "#C5C9C7";
+        };
+
+        normal = {
+          black = "#090E13";
+          red = "#c4746e";
+          green = "#8a9a7b";
+          yellow = "#c4b28a";
+          blue = "#8ba4b0";
+          magenta = "#a292a3";
+          cyan = "#8ea4a2";
+          white = "#c8c093";
+        };
+
+        bright = {
+          black = "#A4A7A4";
+          red = "#e46876";
+          green = "#87a987";
+          yellow = "#e6c384";
+          blue = "#7fb4ca";
+          magenta = "#938aa9";
+          cyan = "#7aa89f";
+          white = "#C5C9C7";
+        };
+
+        selection = {
+          background = "#393B42";
+          foreground = "#C5C9C7";
+        };
+
+        # indexed_colors = {
+        #   index = 16;
+        #   color = "#b6927b";
+        # };
+
+        # indexed_colors = {
+        #   index = 17;
+        #   color = "#b98d7b";
+        # };
+      };
       window.dynamic_padding = true;
       window.padding = {
 	x = 5;
