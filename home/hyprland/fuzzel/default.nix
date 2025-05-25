11d4ebf693fccf1e994cec578e1a1dd6b5a91d6c
Daniel Hwang <daniel@asta.nix>
2025-05-24 23:44:27 -0500

    Update fuzzel colorscheme to Kanso Zen

--- home/hyprland/fuzzel/default.nix
+++ home/hyprland/fuzzel/default.nix
@@ -3,5 +3,14 @@
 {
   programs.fuzzel = {
     enable = true;
+    settings = {
+      colors.background = "090E13FF";
+      colors.text = "C5C9C7FF";
+      colors.match = "938aa9ff";
+      colors.selection-match = "938aa9ff";
+      colors.selection = "393B42FF";
+      colors.selection-text = "C5C9C7FF";
+      colors.border = "c4746eff";
+    };
   };
 }
