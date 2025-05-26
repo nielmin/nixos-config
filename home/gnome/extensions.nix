df9b484db12e1154f4062239751abd522825a635
Daniel Hwang <daniel@asta.nix>
2025-05-26 03:01:14 -0500

    Made extensions toggleable

--- home/gnome/extensions.nix
+++ home/gnome/extensions.nix
@@ -1,7 +1,15 @@
-{ pkgs, ... }:
+{ config, lib, pkgs, ...}:
+with lib;
 
+let
+  cfg = config.home.gnome.extensions; in
 {
-  home.packages = with pkgs; [
-    gnomeExtensions.battery-health-charging
-  ];
+  options = {
+    home.gnome.extensions.enable = lib.mkEnableOption "Enable user GNOME extensions";
+  };
+
+  config = lib.mkIf cfg.enable {
+    home.packages = with pkgs; [
+      gnomeExtensions.battery-health-charging
+    ];
 }
