db63f0a37a4225e5cbe53d350c2a53bb3c24beaa
Daniel Hwang <daniel@asta.nix>
2025-05-26 02:43:55 -0500

    Make common-twm module depend on hyprland module

--- modules/hyprland.nix
+++ modules/hyprland.nix
@@ -4,15 +4,14 @@ with lib;
 let
   cfg = config.desktop.hyprland; in {
 
-  imports = [
-    ./common-twm.nix
-  ];
-
   options ={
     desktop.hyprland.enable = lib.mkEnableOption "Enable Hyprland";
   };
 
   config = lib.mkIf cfg.enable {
+
+    pkgs.twm.enable = true;
+
     programs.hyprland = {
       enable = true;
       portalPackage = pkgs.xdg-desktop-portal-hyprland;
