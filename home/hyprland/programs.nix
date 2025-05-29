8d581372e075e38226b831c08f8bdccbac082e67
Daniel Hwang <daniel@asta.nix>
2025-05-28 20:15:28 -0500

    Add eww to hyprland home module

--- home/hyprland/programs.nix
+++ home/hyprland/programs.nix
@@ -9,16 +9,21 @@ let
   };
 
   config = lib.mkIf cfg.enable {
-    programs.fuzzel = {
-      enable = true;
-      settings = {
-        colors.background = "090E13FF";
-        colors.text = "C5C9C7FF";
-        colors.match = "938aa9ff";
-        colors.selection-match = "938aa9ff";
-        colors.selection = "393B42FF";
-        colors.selection-text = "C5C9C7FF";
-        colors.border = "c4746eff";
+    programs = {
+      fuzzel = {
+        enable = true;
+        settings = {
+          colors.background = "090E13FF";
+          colors.text = "C5C9C7FF";
+          colors.match = "938aa9ff";
+          colors.selection-match = "938aa9ff";
+          colors.selection = "393B42FF";
+          colors.selection-text = "C5C9C7FF";
+          colors.border = "c4746eff";
+        };
+      };
+      eww = {
+        enable = true;
       };
     };
   };
