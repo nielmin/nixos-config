4b7198b9f7ff556cadf248255374a0d69aff9b6d
Daniel Hwang <daniel@asta.nix>
2025-05-31 15:59:14 -0500

    Add custom bash prompt from existing dotfiles

--- home/shell/bash.nix
+++ home/shell/bash.nix
@@ -10,7 +10,11 @@
       ls = "ls --color=auto --group-directories-first";
       ll = "ls -Ahl --color=auto --group-directories-first";
     };
+    initExtra = ''
+      PS1="\[\e[0;34m\]\u\[\e[0;33m\]@\[\e[0;35m\]\h\[\e[0;31m\] \[\e[0;36m\]\w \[\e[0;37m\]$ \[\e[0m\]"
+    '';
     bashrcExtra = ''
+      export PROMPT_DIRTRIM=2
       export PATH="$PATH:$HOME/bin:$HOME/.local/bin:$HOME/go/bin"
     '';
   };
