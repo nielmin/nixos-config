070e3105f11e20575bbcf2db798edf4a172f4386
Daniel Hwang <daniel@asta.nix>
2025-06-01 04:21:44 -0500

    Remove nvf HM module

--- users/daniel/home.nix
+++ users/daniel/home.nix
@@ -11,11 +11,8 @@
     ../../home/services
     ../../home/hyprland
     ../../home/sway
-
-    nvf.homeManagerModules.default
   ];
 
-  nvf.enable = false;
   home.gnome.enable = systemConfig.desktop.gnome.enable or false; 
   home.hyprland.enable = systemConfig.desktop.hyprland.enable or false;
   home.sway.enable = systemConfig.desktop.sway.enable or false;
