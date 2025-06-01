652d9415760d15b6e5e10deee25cdd9fd0f5c99b
Daniel Hwang <daniel@asta.nix>
2025-06-01 00:02:46 -0500

    Initial hm sway config

---
+++ home/sway.nix
@@ -0,0 +1,25 @@
+{ config, lib, pkgs, ... }:
+with lib;
+
+let
+  cfg = config.home.sway; in {
+
+  imports = [
+    ../programs/twm.nix
+    ../services/twm.nix
+  ];
+
+  options = {
+    home.sway.enable = lib.mkEnableOption "Enable sway home-manager";
+  };
+
+  config = lib.mkIf cfg.enable {
+    home.twm.services.enable = true;
+    home.twm.programs.enable = true;
+
+    wayland.windowManager.sway = {
+      enable = true;
+      checkConfig = true;
+    };
+  };
+}
