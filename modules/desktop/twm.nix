3c5e462b22c5a24001cf64a74b823d655202da72
Daniel Hwang <daniel@asta.nix>
2025-06-01 12:13:09 -0500

    Add UWSM Hyprland entry

--- modules/desktop/twm.nix
+++ modules/desktop/twm.nix
@@ -12,6 +12,11 @@ let
     programs.uwsm = {
       enable = true;
       waylandCompositors = {
+        hyprland = {
+          prettyName = "Hyprland";
+          comment = "Hyprland compositor managed by UWSM";
+          binPath = "/run/current-system/sw/bin/Hyprland";
+        };
         sway = {
           prettyName = "Sway";
           comment = "Sway compositor managed by UWSM";
