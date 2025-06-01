2be769ce39887b67b22034037f03bdf72350aefc
Daniel Hwang <daniel@asta.nix>
2025-06-01 11:12:20 -0500

    Add charging threshhold for 2nd battery

--- modules/system.nix
+++ modules/system.nix
@@ -85,6 +85,9 @@
     settings = {
       START_CHARGE_THRESH_BAT0 = 60;
       STOP_CHARGE_THRESH_BAT0 = 85;
+
+      START_CHARGE_THRESH_BAT1 = 60;
+      STOP_CHARGE_THRESH_BAT1 = 85;
     };
   };
 
