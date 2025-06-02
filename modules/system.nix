ea4c8e09343f308c79b9742fd433dbcc1747db14
Daniel Hwang <daniel@asta.nix>
2025-06-01 20:12:00 -0500

    Move tlp to separate module

--- modules/system.nix
+++ modules/system.nix
@@ -61,17 +61,6 @@
     printing.enable = true;
 
     thermald.enable = true;
-
-    tlp = {
-      enable = true;
-      settings = {
-        START_CHARGE_THRESH_BAT0 = 60;
-        STOP_CHARGE_THRESH_BAT0 = 85;
-
-        START_CHARGE_THRESH_BAT1 = 60;
-        STOP_CHARGE_THRESH_BAT1 = 85;
-      };
-    };
   };
 
   fonts.packages = with pkgs; [
