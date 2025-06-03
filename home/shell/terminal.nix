14f37f1861f88d2eb483c454c51d903ccef58575
Daniel Hwang <daniel@asta.nix>
2025-06-02 21:46:38 -0500

    Disable nvim theme for catppuccin-nix

--- home/shell/terminal.nix
+++ home/shell/terminal.nix
@@ -11,47 +11,47 @@ in
     enable = true;
     # custom settings
     settings = {
-      colors = {
-        # Zenbones Zenwritten Dark
+      # colors = {
+      #   # Zenbones Zenwritten Dark
 
-        primary = {
-          foreground = "#bbbbbb";
-          background = "#191919";
-        };
+      #   primary = {
+      #     foreground = "#bbbbbb";
+      #     background = "#191919";
+      #   };
 
-        cursor = {
-          text = "#191919";
-          cursor = "#c9c9c9";
-        };
+      #   cursor = {
+      #     text = "#191919";
+      #     cursor = "#c9c9c9";
+      #   };
 
-        selection = {
-          text = "#bbbbbb";
-          background = "#404040";
-        };
+      #   selection = {
+      #     text = "#bbbbbb";
+      #     background = "#404040";
+      #   };
 
-        normal = {
-          black = "#191919";
-          red = "#de6e7c";
-          green = "#819b69";
-          yellow = "#b77e64";
-          blue = "#6099c0";
-          magenta = "#b279a7";
-          cyan = "#66a5ad";
-          white = "#bbbbbb";
-        };
+      #   normal = {
+      #     black = "#191919";
+      #     red = "#de6e7c";
+      #     green = "#819b69";
+      #     yellow = "#b77e64";
+      #     blue = "#6099c0";
+      #     magenta = "#b279a7";
+      #     cyan = "#66a5ad";
+      #     white = "#bbbbbb";
+      #   };
 
-        bright = {
-          black = "#3d3839";
-          red = "#e8838f";
-          green = "#8bae68";
-          yellow = "#d68c67";
-          blue = "#61abda";
-          magenta = "#cf86c1";
-          cyan = "#65b8c1";
-          white = "#8e8e8e";
-        };
+      #   bright = {
+      #     black = "#3d3839";
+      #     red = "#e8838f";
+      #     green = "#8bae68";
+      #     yellow = "#d68c67";
+      #     blue = "#61abda";
+      #     magenta = "#cf86c1";
+      #     cyan = "#65b8c1";
+      #     white = "#8e8e8e";
+      #   };
 
-      };
+      # };
       window.dynamic_padding = true;
       window.padding = {
 	x = 5;
