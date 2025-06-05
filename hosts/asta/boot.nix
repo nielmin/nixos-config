2941345eda04edb7d3e86f55a355cc1aa8e4b520
Daniel Hwang <daniel@asta.nix>
2025-06-04 20:38:07 -0500

    Refactor boot module

--- hosts/asta/boot.nix
+++ hosts/asta/boot.nix
@@ -1,26 +1,29 @@
 { config, pkgs, lib, ... }:
 
 {
+  boot = {
+    kernelModules = [ "kvm-intel" ];
+    extraModulePackages = [ ];
 
-  boot.initrd.availableKernelModules = [ "xhci_pci" "ahci" "usb_storage" "sd_mod" ];
-  boot.initrd.kernelModules = [ ];
-  boot.kernelModules = [ "kvm-intel" ];
-  boot.extraModulePackages = [ ];
+    initrd = {
+      availableKernelModules = [ "xhci_pci" "ahci" "usb_storage" "sd_mod" ];
+      kernelModules = [ ];
+      luks.devices."luks-6b83a834-d164-4db5-bb64-139afd3c9731".device = "/dev/disk/by-uuid/6b83a834-d164-4db5-bb64-139afd3c9731";
+      systemd.enable = true;
+    };
 
-  boot.initrd.luks.devices."luks-6b83a834-d164-4db5-bb64-139afd3c9731".device = "/dev/disk/by-uuid/6b83a834-d164-4db5-bb64-139afd3c9731";
+    # Bootloader.
+    loader = {
+      systemd-boot.enable = lib.mkForce false;
+      systemd-boot.configurationLimit = 5;
+      efi.canTouchEfiVariables = true;
+    };
 
-  boot.bootspec.enable = true;
+    bootspec.enable = true;
 
-  boot.initrd.systemd.enable = true;
-
-  # Bootloader.
-  boot.loader.systemd-boot.enable = lib.mkForce false;
-  boot.loader.systemd-boot.configurationLimit = 5;
-  boot.loader.efi.canTouchEfiVariables = true;
-
-  boot.lanzaboote = {
-    enable = true;
-    pkiBundle = "/var/lib/sbctl";
+    lanzaboote = {
+      enable = true;
+      pkiBundle = "/var/lib/sbctl";
+    };
   };
-
 }
