88db8bfc6eb9be1d75941a54f2224b82d56d1767
Daniel Hwang <daniel@asta.nix>
2025-06-06 05:01:19 -0500

    Set fuzzel theme to catppuccin

--- home/programs/twm.nix
+++ home/programs/twm.nix
@@ -9,6 +9,8 @@ let
   };
 
   config = lib.mkIf cfg.enable {
+    catppuccin.fuzzel.enable = true;
+
     programs = {
       fuzzel = {
         enable = true;
@@ -16,13 +18,6 @@ let
           main = {
             icon-theme = "MoreWaita";
           };
-          colors.background = "090E13FF";
-          colors.text = "C5C9C7FF";
-          colors.match = "938aa9ff";
-          colors.selection-match = "938aa9ff";
-          colors.selection = "393B42FF";
-          colors.selection-text = "C5C9C7FF";
-          colors.border = "c4746eff";
         };
       };
       eww = {
