133b71ee729a1e83b0f2b234b7382f05d4c68fe9
Daniel Hwang <daniel@asta.nix>
2025-06-06 05:03:56 -0500

    Replace fnott with mako (catppuccin)

--- home/services/twm.nix
+++ home/services/twm.nix
@@ -9,6 +9,8 @@ let
   };
 
   config = lib.mkIf cfg.enable {
+    catppuccin.mako.enable = true;
+
     services = {
       swayosd = {
         enable = true;
@@ -22,60 +24,8 @@ let
           night = 4500;
         };
       };
-      fnott = {
+      mako = {
         enable = true;
-        settings = {
-
-          main = {
-            min-width = 0;
-            max-width = 440;
-            max-height = 0;
-            stacking-order = "bottom-up";
-            anchor = "top-right";
-            edge-margin-vertical = 10;
-            edge-margin-horizontal = 10;
-            notification-margin = 10;
-            icon-theme = "Papirus-Dark";
-            max-icon-size = 36;
-
-            layer = "overlay";
-            background = "100F0Fff";
-            border-color = "282726ff";
-            border-radius = 0;
-            border-size = 1;
-
-            padding-vertical = 15;
-            padding-horizontal = 15;
-            dpi-aware = "no";
-
-            title-font = "JetBrainsMono Nerd Font Regular:pixelsize=14";
-            title-color = "24837Bff";
-            title-format = "%a%A";
-
-            summary-font = "JetBrainsMono Nerd Font Regular:pixelsize=14";
-            summary-color = "FFFCF0ff";
-            summary-format = "<b>%s</b>";
-
-            body-font = "JetBrainsMono Nerd Font Regular:pixelsize=14";
-            body-color = "CECDC3ff";
-            body-format = "%b";
-
-            progress-bar-height = 20;
-            progress-bar-color = "CECDC3ff";
-
-            max-timeout = 0;
-            default-timeout = 0;
-            idle-timeout = 0;
-          };
-
-          critical = {
-            border-color = "A02F6Fff";
-            title-color = "A02F6Fff";
-            body-color = "ffffffff";
-            summary-color = "AD8301ff";
-            background = "000000ff";
-          };
-        };
       };
       kanshi = {
         enable = true;
