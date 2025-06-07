97c4c55c876840ef8c451b899326b648cd03fbbd
Daniel Hwang <daniel@asta.nix>
2025-06-07 04:32:54 -0500

    Set new windows as slave

--- home/hyprland/settings.nix
+++ home/hyprland/settings.nix
@@ -62,6 +62,10 @@
       preserve_split = true; # You probably want this
     };
 
+    master = {
+      new_status = "slave";
+    };
+
     misc = {
       force_default_wallpaper = 0; # Set to 0 or 1 to disable the anime mascot wallpapers
       disable_hyprland_logo = true; # If true disables the random hyprland logo / anime girl background. :(
