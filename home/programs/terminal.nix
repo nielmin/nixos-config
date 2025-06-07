8e34eaaf7d05945b5191d2ba0b6f8dfe5d897076
Daniel Hwang <daniel@asta.nix>
2025-06-06 20:36:11 -0500

    Remove commented lines

--- home/programs/terminal.nix
+++ home/programs/terminal.nix
@@ -4,54 +4,11 @@ let
   font = "Fira Code Nerd Font";
 in
 {
-  # alacritty - a cross-platform, GPU-accelerated terminal emulator
   catppuccin.alacritty.enable = true;
 
   programs.alacritty = {
     enable = true;
-    # custom settings
     settings = {
-      # colors = {
-      #   # Zenbones Zenwritten Dark
-
-      #   primary = {
-      #     foreground = "#bbbbbb";
-      #     background = "#191919";
-      #   };
-
-      #   cursor = {
-      #     text = "#191919";
-      #     cursor = "#c9c9c9";
-      #   };
-
-      #   selection = {
-      #     text = "#bbbbbb";
-      #     background = "#404040";
-      #   };
-
-      #   normal = {
-      #     black = "#191919";
-      #     red = "#de6e7c";
-      #     green = "#819b69";
-      #     yellow = "#b77e64";
-      #     blue = "#6099c0";
-      #     magenta = "#b279a7";
-      #     cyan = "#66a5ad";
-      #     white = "#bbbbbb";
-      #   };
-
-      #   bright = {
-      #     black = "#3d3839";
-      #     red = "#e8838f";
-      #     green = "#8bae68";
-      #     yellow = "#d68c67";
-      #     blue = "#61abda";
-      #     magenta = "#cf86c1";
-      #     cyan = "#65b8c1";
-      #     white = "#8e8e8e";
-      #   };
-
-      # };
       window.dynamic_padding = true;
       window.padding = {
 	x = 5;
