60795f4e5f3b90c3dae43740edb8636386623e0d
Daniel Hwang <daniel@asta.nix>
2025-06-07 03:01:33 -0500

    Remove hyprsunset from services

--- home/hyprland/services.nix
+++ home/hyprland/services.nix
@@ -39,23 +39,6 @@ let
           wallpaper = [ ",~/nixos-config/wallpapers/slavic-town.jpg" ];
         };
       };
-      hyprsunset = {
-        enable = true;
-        transitions = {
-          sunrise = {
-            calendar = "*-*-* 06:00:00";
-            requests = [
-              [ "temperature" "6500" ]
-            ];
-          };
-          sunset = {
-            calendar = "*-*-* 19:00:00";
-            requests = [
-              [ "temperature" "3500" ]
-            ];
-          };
-        };
-      };
     };
   };
 }
