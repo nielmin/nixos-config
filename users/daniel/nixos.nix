f934fa0bc2dc9d7c841f76b8953767c8ba4b0eda
Daniel Hwang <dawh@ines.home>
2025-06-23 20:37:40 -0500

    Add public ssh key for asta

---
+++ users/daniel/nixos.nix
@@ -0,0 +1,7 @@
+{
+  users.users.daniel = {
+    openssh.authorizedKeys.keys = [
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHKmr//V3GIOxwtWLy/KU8Qnj7BaGqtUJratYpKqkWvy daniel@asta"
+    ];
+  };
+}
