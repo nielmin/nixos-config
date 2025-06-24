e121c26a090d6b26afe41b20e2c4b11d995565b1
Daniel Hwang <dawh@ines.home>
2025-06-23 23:02:24 -0500

    Initial hardware-configuration generated by nixos-anywhere

--- hosts/asta/hardware-configuration.nix
+++ hosts/asta/hardware-configuration.nix
@@ -8,19 +8,10 @@
     [ (modulesPath + "/installer/scan/not-detected.nix")
     ];
 
-  fileSystems."/" =
-    { device = "/dev/disk/by-uuid/2d813cb7-9477-4d10-a147-f8183e8887d3";
-      fsType = "btrfs";
-      options = [ "subvol=@" ];
-    };
-
-  fileSystems."/boot" =
-    { device = "/dev/disk/by-uuid/8E85-AAD9";
-      fsType = "vfat";
-      options = [ "fmask=0077" "dmask=0077" ];
-    };
-
-  swapDevices = [ ];
+  boot.initrd.availableKernelModules = [ "xhci_pci" "ahci" "usb_storage" "sd_mod" ];
+  boot.initrd.kernelModules = [ ];
+  boot.kernelModules = [ "kvm-intel" ];
+  boot.extraModulePackages = [ ];
 
   # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
   # (the default) this is the recommended approach. When using systemd-networkd it's
