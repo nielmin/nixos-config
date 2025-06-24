5e5974adb8def5ece12d474bfd5db19b587a90f7
Daniel Hwang <dawh@ines.home>
2025-06-24 18:10:13 -0500

    Add keys for daniel@asta

---
+++ secrets/secrets.nix
@@ -0,0 +1,13 @@
+  let
+    # Users
+    daniel = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHKmr//V3GIOxwtWLy/KU8Qnj7BaGqtUJratYpKqkWvy daniel@asta";
+    users = [ daniel ];
+
+    # Hosts
+    asta = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGQWWo40J1Sab/qD0MEJUhFRM6mJ1Ca9sM9vUxsBhdtv root@asta";
+    systems = [ asta ];
+  in
+  {
+    "user-passwd.age".publicKeys = [ daniel asta ];
+    "wifi-passwd.age".publicKeys = users ++ systems;
+  }
