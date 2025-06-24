7e4d3050e399af4c64b5c60ff9cdcd829ecd987a
Daniel Hwang <dawh@ines.home>
2025-06-23 23:05:14 -0500

    Remove luks device

--- hosts/asta/boot.nix
+++ hosts/asta/boot.nix
@@ -8,7 +8,6 @@
     initrd = {
       availableKernelModules = [ "xhci_pci" "ahci" "usb_storage" "sd_mod" ];
       kernelModules = [ ];
-      luks.devices."luks-6b83a834-d164-4db5-bb64-139afd3c9731".device = "/dev/disk/by-uuid/6b83a834-d164-4db5-bb64-139afd3c9731";
       systemd.enable = true;
     };
 
