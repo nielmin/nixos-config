8503c92fc5cfcb5aa7d363f2416379ca914588c1
Daniel Hwang <dawh@ines.home>
2025-06-25 16:03:14 -0500

    Format flake.nix

--- flake.nix
+++ flake.nix
@@ -4,88 +4,88 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
 
-# disko
+    # disko
     disko = {
       url = "github:nix-community/disko";
     };
 
-# Home Manager
+    # Home Manager
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
-# Lanzaboote
+    # Lanzaboote
     lanzaboote = {
       url = "github:nix-community/lanzaboote/v0.4.2";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
-# NixOS Hardware
+    # NixOS Hardware
     nixos-hardware = {
       url = "github:NixOS/nixos-hardware/master";
     };
 
-# stylix
+    # stylix
     stylix = {
       url = "github:danth/stylix";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
-# catppuccin
+    # catppuccin
     catppuccin = {
       url = "github:catppuccin/nix";
     };
 
-# agenix
+    # agenix
     agenix = {
       url = "github:ryantm/agenix";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
 
-  outputs = { 
-    self, 
-    nixpkgs, 
-    home-manager, 
-    nixos-hardware, 
-    stylix, 
-    catppuccin, 
-    lanzaboote, 
-    disko, 
-    agenix, 
-    ... 
-  }@inputs: {
+  outputs = {
+    self,
+    nixpkgs,
+    home-manager,
+    nixos-hardware,
+    stylix,
+    catppuccin,
+    lanzaboote,
+    disko,
+    agenix,
+    ...
+  } @ inputs: {
     nixosConfigurations = {
       asta = let
         username = "daniel";
-      specialArgs = { inherit username catppuccin; };
+        specialArgs = {inherit username catppuccin;};
       in
         nixpkgs.lib.nixosSystem {
           inherit specialArgs;
           system = "x86_64-linux";
           modules = [
             catppuccin.nixosModules.catppuccin
-              lanzaboote.nixosModules.lanzaboote
-              nixos-hardware.nixosModules.lenovo-thinkpad-t480
-              disko.nixosModules.disko
-              agenix.nixosModules.default
-              ./hosts/asta
-              ./users/${username}/nixos.nix
+            lanzaboote.nixosModules.lanzaboote
+            nixos-hardware.nixosModules.lenovo-thinkpad-t480
+            disko.nixosModules.disko
+            agenix.nixosModules.default
+            ./hosts/asta
+            ./users/${username}/nixos.nix
 
-              home-manager.nixosModules.home-manager
-              ({ config, ... }: {
-               home-manager.useGlobalPkgs = true;
-               home-manager.useUserPackages = true;
+            home-manager.nixosModules.home-manager
+            ({config, ...}: {
+              home-manager.useGlobalPkgs = true;
+              home-manager.useUserPackages = true;
 
-               home-manager.extraSpecialArgs = inputs // specialArgs // { systemConfig = config; };
-               home-manager.users.${username} = import ./users/${username}/home.nix;
-               })
+              home-manager.extraSpecialArgs = inputs // specialArgs // {systemConfig = config;};
+              home-manager.users.${username} = import ./users/${username}/home.nix;
+            })
           ];
         };
       nuc = let
         username = "daniel";
-      specialArgs = { inherit username; };
+        specialArgs = {inherit username;};
       in
         nixpkgs.lib.nixosSystem {
           inherit specialArgs;
@@ -93,14 +93,14 @@
           modules = [
             ./hosts/nuc
 
-              home-manager.nixosModules.home-manager
-              ({ config, ...}: {
-               home-manager.useGlobalPkgs = true;
-               home-manager.useUserPackages = true;
+            home-manager.nixosModules.home-manager
+            ({config, ...}: {
+              home-manager.useGlobalPkgs = true;
+              home-manager.useUserPackages = true;
 
-               home-manager.extraSpecialArgs = inputs // specialArgs // { systemConfig = config; };
-               home-manager.users.${username} = import ./users/${username}/home.nix;
-               })
+              home-manager.extraSpecialArgs = inputs // specialArgs // {systemConfig = config;};
+              home-manager.users.${username} = import ./users/${username}/home.nix;
+            })
           ];
         };
     };
