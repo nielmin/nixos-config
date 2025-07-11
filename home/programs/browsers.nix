cd1a0630a6eb853adfb04565e98072094e48e540
Daniel Hwang <daniel@asta.nix>
2025-07-10 22:25:56 -0500

    Add default profile options

--- home/programs/browsers.nix
+++ home/programs/browsers.nix
@@ -3,9 +3,21 @@
 {
   programs.firefox = {
     enable = true;
+    profiles = {
+      default = {
+        name = "default";
+        isDefault = true;
+        containers = {
+          shopping = {
+            color = "green";
+            icon = "cart";
+            id = 1;
+          };
+        };
+      };
+    };
     nativeMessagingHosts = [
       pkgs.keepassxc
     ];
-    profiles.default.extensions.force = true;
   };
 }
