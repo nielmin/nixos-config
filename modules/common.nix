f5c8133137fd622b897f7f3d291f254915760a98
Daniel Hwang <dawh@ines.home>
2025-07-21 13:17:56 -0500

    Move common system pkgs to common.nix

--- modules/common.nix
+++ modules/common.nix
@@ -1,6 +1,9 @@
 { config, lib, pkgs, ...}:
 {
   environment.systemPackages = with pkgs; [
-    firefox
+    neovim 
+    git
+    curl
+    wget
   ];
 }
--- modules/system.nix
+++ modules/system.nix
@@ -66,10 +66,6 @@
   };
 
   environment.systemPackages = with pkgs; [
-    neovim 
-    git
-    curl
-    wget
     efibootmgr
     dconf
     acpi
