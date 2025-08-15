3ea01870612125c77fc5f32290269d16d1a36366
Daniel Hwang <dawh@ines.home>
2025-08-15 14:40:14 -0500

    Remove default comments

--- hosts/vm/default.nix
+++ hosts/vm/default.nix
@@ -15,24 +15,6 @@
     ];
 
   networking.hostName = "vm"; # Define your hostname.
-  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
-
-  # Configure network proxy if necessary
-  # networking.proxy.default = "http://user:password@proxy:port/";
-  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
-
-  # Enable touchpad support (enabled default in most desktopManager).
-  # services.xserver.libinput.enable = true;
-  
-  # Define a user account. Don't forget to set a password with ‘passwd’.
-
-  # Some programs need SUID wrappers, can be configured further or are
-  # started in user sessions.
-  # programs.mtr.enable = true;
-  # programs.gnupg.agent = {
-  #   enable = true;
-  #   enableSSHSupport = true;
-  # };
   
   # List services that you want to enable:
   
