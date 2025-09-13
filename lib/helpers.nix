84c8374c26d3e195eff7d351a8b2ae21dfee9b2b
Daniel Hwang <dawh@ines.home>
2025-09-12 23:51:59 -0500

    Add home config helper

--- lib/helpers.nix
+++ lib/helpers.nix
@@ -1,46 +1,35 @@
-{ nixpkgs, overlays, inputs }:
-
-name:
 {
-  system,
-  user,
-  darwin ? false,
-  wsl ? false
+  self,
+  inputs,
+  outputs,
+  stateVersion,
+  username,
+  ...
 }:
-
-let
-  isLinux = !darwin;
-
-  hostConfig = ../hosts/${name};
-  userOSConfig = ../users/${user}/nixos.nix;
-  userHMConfig = ../users/${user}/home.nix
-
-  systemFunc = nixpkgs.lib.nixosSystem;
-  home-manager = inputs.home-manager.nixosModules;
-
-in systemFunc rec {
-  inherit system;
-
-  modules = [
-    machineConfig
-    userOSConfig
-    home-manager.home-manager {
-      home-manager.useGlobalPkgs = true;
-      home-manager.useUserPackages = true;
-      home-manager.users.${user} = import userHMConfig {
-        inputs = inputs;
+{
+  mkHome =
+    {
+      hostname,
+      user ? username,
+      desktop ? null,
+      system ? "x86_64-linux",
+    }:
+    inputs.home-manager.lib.homeManagerConfiguration {
+      pkgs = inputs.nixpkgs.legacyPackages.${system};
+      extraSpecialArgs = {
+        inherit
+          self
+          inputs
+          outputs
+          stateVersion
+          hostname
+          desktop
+          ;
+        username = user;
       };
-    }
-
-
-  {
-    config._module.args = {
-      currentSystem = system;
-      currentSystemName = name;
-      currentSystemUser = user;
-      inputs = inputs;
+      modules = [
+        inputs.catppuccin.homeManagerModules.catppuccin
+        ../users/${username}/home.nix
+      ];
     };
-  }
-  ];
 }
-  
