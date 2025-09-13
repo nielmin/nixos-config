9bac153499935ef7e695bc79d1ca72e857bbff62
Daniel Hwang <dawh@ines.home>
2025-09-13 00:20:46 -0500

    Add default.nix for helper functions

---
+++ lib/default.nix
@@ -0,0 +1,23 @@
+{
+  self,
+  inputs,
+  outputs,
+  stateVersion,
+  username,
+  ...
+}:
+
+let
+  helpers = import ./helpers.nix {
+    inherit
+      self
+      inputs
+      outputs
+      stateVersion
+      username
+      ;
+  };
+in
+{
+  inherit (helpers) mkHome mkHost forAllSystems;
+}
