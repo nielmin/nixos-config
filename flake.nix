3f449e0c85d76842e81d7f03e14f9a63c02a2d7c
Daniel Hwang <dawh@ines.home>
2025-09-14 14:31:38 -0500

    Initial dendritic flake

--- flake.nix
+++ flake.nix
@@ -36,96 +36,15 @@
       url = "github:ryantm/agenix";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+
+    # flake-parts
+    flake-parts.url = "github:hercules-ci/flake-parts";
+
+    # import-tree
+    import-tree.url = "github:vic/import-tree";
   };
 
-  outputs = {
-    self,
-    nixpkgs,
-    home-manager,
-    nixos-hardware,
-    catppuccin,
-    lanzaboote,
-    disko,
-    agenix,
-    ...
-  } @ inputs:
-    let
-      inherit (self) outputs;
-      stateVersion = "24.11";
-      username = "daniel";
-      specialArgs = {inherit username catppuccin;};
-
-      libx = import ./lib/helpers.nix {
-        inherit
-          self
-          inputs
-          outputs
-          stateVersion
-          username
-          ;
-      };
-
-    allSystems = [
-      "x86_64-linux"
-      "aarch64-linux"
-      "x86_64-darwin"
-      "aarch64-darwin"
-    ];
-
-    forAllSystems = f:
-      nixpkgs.lib.genAttrs allSystems (system:
-        f {
-          pkgs = import nixpkgs {inherit system;};
-        });
-  in {
-    homeConfigurations = {
-      "${username}@vm" = libx.mkHome {
-        hostname = "vm";
-      };
-    };
-
-    nixosConfigurations = {
-      vm = libx.mkHost {
-        hostname = "vm";
-      };
-
-      nuc = libx.mkHost {
-        hostname = "nuc";
-      };
-
-      asta = nixpkgs.lib.nixosSystem {
-        inherit specialArgs;
-        system = "x86_64-linux";
-        modules = [
-          catppuccin.nixosModules.catppuccin
-          lanzaboote.nixosModules.lanzaboote
-          nixos-hardware.nixosModules.lenovo-thinkpad-t480
-          disko.nixosModules.disko
-          agenix.nixosModules.default
-          ./hosts/asta
-          ./users/${username}/nixos.nix
-
-          home-manager.nixosModules.home-manager
-          ({config, ...}: {
-            home-manager.useGlobalPkgs = true;
-            home-manager.useUserPackages = true;
-
-            home-manager.extraSpecialArgs = inputs // specialArgs // {systemConfig = config;};
-            home-manager.users.${username} = import ./users/${username}/home.nix;
-          })
-        ];
-      };
-    };
-
-    devShells = forAllSystems ({pkgs}: {
-      default = pkgs.mkShell {
-        packages = with pkgs; [
-          jujutsu
-          alejandra
-        ];
-      };
-    });
-
-    formatter = forAllSystems ({pkgs}: pkgs.alejandra);
-  };
+  outputs =
+  { ... }@inputs:
+    inputs.flake-parts.lib.mkFlake { inherit inputs; } (inputs.import-tree ./modules);
 }
