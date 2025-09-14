5b5400bfde60ca92a68ed6dd61d6a1c88b141284
Daniel Hwang <dawh@ines.home>
2025-09-14 17:27:51 -0500

    Add existing helix module

---
+++ newModules/home/core/programs/helix.nix
@@ -0,0 +1,22 @@
+{
+  flake.modules.homeManager.core =
+    { config, ... }:
+    {
+      programs.helix = {
+        enable = true;
+
+        settings = {
+          editor = {
+            line-number = "relative";
+            lsp.display-messages = true;
+          };
+
+          keys.normal = {
+            space.space = "file_picker";
+            space.w = ":w";
+            space.q = ":q";
+          };
+        };
+      };
+    };  
+}
