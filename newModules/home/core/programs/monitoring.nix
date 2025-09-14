263882590e78038f68f312fb93dcc38374aafaf7
Daniel Hwang <dawh@ines.home>
2025-09-14 17:08:36 -0500

    Initial home monitoring module

---
+++ newModules/home/core/programs/monitoring.nix
@@ -0,0 +1,20 @@
+{
+  flake.modules.homeManager.core = {
+    programs = {
+      bottom.enable = true;
+      htop.enable = true;
+
+      btop = {
+        enable = true;
+
+        settings = {
+          color_theme = "gruvbox_dark";
+          update_ms = 1000;
+          disks_filter = "";
+          mem_graphs = false;
+          proc_per_core = true;
+        };
+      };
+    };
+  };
+}
