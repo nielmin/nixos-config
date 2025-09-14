7cb603cd7e6ffc48f2e8e601355cee623a2e6004
Daniel Hwang <dawh@ines.home>
2025-09-14 15:09:47 -0500

    Initial hosts module

---
+++ newModules/flake/nixos-hosts.nix
@@ -0,0 +1,51 @@
+{
+  inputs,
+  lib,
+  config,
+  ...
+}:
+
+let
+  inherit (lib) types mkOption;
+in
+{
+  options = {
+    nixosHosts =
+      let
+        nixosHostType = types.submodule {
+          options = {
+            system = mkOption {
+              type = types.str;
+              default = "x86_64-linux";
+            };
+
+            unstable = lib.mkOption {
+              type = types.bool;
+            };
+          };
+        };
+      in
+      mkOption {
+        type = types.attrsOf nixosHostType;
+      };
+  };
+
+  config = {
+    flake.nixosConfigurations =
+      let
+        mkHost =
+          hostname: options:
+          let
+            nixpkgs' = if options.unstable then inputs.nixpkgs else inputs.nixpkgs-unstable;
+          in
+            nixpkgs'.lib.nixosSystem {
+              inherit (options) system;
+              modules = [
+                config.flake.modules.nixos.core
+                (config.flake.modules.nixos."host_${hostname}" or {})
+              ];
+            };
+      in
+      lib.mapAttrs mkHost config.nixosHosts;
+  };
+}
