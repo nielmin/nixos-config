7e10dd1f7c8b51d59c5cd9a2bd6a2079be9de558
Daniel Hwang <dawh@ines.home>
2025-09-14 20:23:03 -0500

    Add existing font pkgs

---
+++ newModules/nixos/desktop/fonts.nix
@@ -0,0 +1,15 @@
+{
+  flake.modules.nixos.fonts = {
+    
+    fonts.packages = with pkgs; [
+      noto-fonts
+      noto-fonts-cjk-sans
+      noto-fonts-emoji
+
+      inter
+
+      nerd-fonts.fira-code
+      nerd-fonts.jetbrains-mono
+    ];
+  }; 
+}
