85bfbb2af37c24258d3d8d4d42495b5552d01fc7
Daniel Hwang <dawh@ines.home>
2025-09-16 15:41:43 -0500

    Initial nixos sway module

---
+++ newModules/nixos/desktop/sway.nix
@@ -0,0 +1,32 @@
+{
+  flake.modules.nixos.desktop =
+    { pkgs, ... }:
+
+    {
+    programs.sway = {
+      enable = true;
+      wrapperFeatures.gtk = true;
+    };
+
+    services.dbus.enable = true;
+
+    services.gnome.gnome-keyring.enable = true;
+
+    security.pam.loginLimits = [
+      { domain = "@users"; item = "rtprio"; type = "-"; value = 1; }
+    ];
+
+    environment.systemPackages = with pkgs; [
+      mako
+    ];
+
+    xdg.portal = {
+      enable = true;
+      wlr.enable = true;
+      extraPortals = with pkgs; [
+        xdg-desktop-portal-gtk
+      ];
+      config.common.default = "*";
+    };
+  };
+}
