8f930206749a9c5ecbea1e01387c6dffacf1c6d3
Daniel Hwang <dawh@ines.home>
2025-09-15 21:50:49 -0500

    Add hm hyprland window rules

--- newModules/home/desktop/hyprland/window-rules.nix
+++ newModules/home/desktop/hyprland/window-rules.nix
@@ -1,23 +1,23 @@
-{ pkgs, ... }:
-
 {
-  wayland.windowManager.hyprland.settings.windowrule = [
-    "nofocus,class:^$,title:^$,xwayland:1,floating:1,fullscreen:0,pinned:0"
+  flake.modules.homeManager.hyprland = {
+    wayland.windowManager.hyprland.settings.windowrule = [
+      "nofocus,class:^$,title:^$,xwayland:1,floating:1,fullscreen:0,pinned:0"
 
-    "float,class:^(nwg-look)$,title:^(nwg-look)$"
-    "size 40% 40%,class:^(nwg-look)$,title:^(nwg-look)$"
+      "float,class:^(nwg-look)$,title:^(nwg-look)$"
+      "size 40% 40%,class:^(nwg-look)$,title:^(nwg-look)$"
 
-    "center,class:^(firefox)$,title:^(firefox)$"
-    "size 30% 30%,class:^(firefox)$,title:^(firefox)$"
+      "center,class:^(firefox)$,title:^(firefox)$"
+      "size 30% 30%,class:^(firefox)$,title:^(firefox)$"
 
-    # Ignore maximize requests from apps. You'll probably like this.
-    "suppressevent maximize, class:.*"
+      # Ignore maximize requests from apps. You'll probably like this.
+      "suppressevent maximize, class:.*"
 
-    # Fix some dragging issues with XWayland
-    "nofocus,class:^$,title:^$,xwayland:1,floating:1,fullscreen:0,pinned:"
+      # Fix some dragging issues with XWayland
+      "nofocus,class:^$,title:^$,xwayland:1,floating:1,fullscreen:0,pinned:"
 
-    "center, class:xdg-desktop-portal-gtk"
-    "float, class:xdg-desktop-portal-gtk"
-    "size 50% 50%, class:xdg-desktop-portal-gtk"
-  ];
+      "center, class:xdg-desktop-portal-gtk"
+      "float, class:xdg-desktop-portal-gtk"
+      "size 50% 50%, class:xdg-desktop-portal-gtk"
+    ];
+  };
 }
