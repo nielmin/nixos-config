e53de260b2aad44c0184a987b0e5a46228d0b279
Daniel Hwang <dawh@ines.home>
2025-09-15 21:23:32 -0500

    Add nixos desktop hyprland

---
+++ newModules/nixos/desktop/hyprland.nix
@@ -0,0 +1,27 @@
+{
+  flake.modules.nixos.desktop =
+    { pkgs, ... }:
+    {
+      
+    programs.hyprland = {
+      enable = true;
+      portalPackage = pkgs.xdg-desktop-portal-hyprland;
+      xwayland.enable = true;
+    };
+
+    environment.systemPackages = with pkgs; [
+      hyprpaper
+      hypridle
+      hyprsunset
+      hyprlock
+    ];
+
+    xdg.portal = {
+      enable = true;
+      extraPortals = with pkgs; [
+        xdg-desktop-portal-gtk
+      ];
+      config.common.default = "*";
+    };
+  };
+}
