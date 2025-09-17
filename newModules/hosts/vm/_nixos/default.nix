09018ca13e43c6bba69f8ff2145fa15df4d62f73
Daniel Hwang <dawh@ines.home>
2025-09-16 21:24:02 -0500

    Remove duplicate ssh config

--- newModules/hosts/vm/_nixos/default.nix
+++ newModules/hosts/vm/_nixos/default.nix
@@ -9,12 +9,5 @@
 
   networking.hostName = "vm";
     
-  services.openssh.enable = true;
-
-  users.users.root.openssh.authorizedKeys.keys =
-  [
-    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPRkbcVwMaIfasq2qg6/ZsNz8i37JPUQHZJPBU6c80p1 dawh@ines"
-  ];
-
   system.stateVersion = "24.11"; # Did you read the comment?
 }
