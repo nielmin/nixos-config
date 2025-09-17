636fcf0438cb9f7dea499987522431b18b80bcb2
Daniel Hwang <daniel@asta.nix>
2025-09-17 04:17:22 -0500

    Import lanzaboote to core

--- newModules/nixos/core/imports.nix
+++ newModules/nixos/core/imports.nix
@@ -2,6 +2,7 @@
 {
   flake.modules.nixos.core.imports = with config.flake.modules.nixos; [
     inputs.disko.nixosModules.disko
+    inputs.lanzaboote.nixosModules.lanzaboote
 
     bootloader
     nix
