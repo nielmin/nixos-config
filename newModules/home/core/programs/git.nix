955558a9657a95ad0e764a28acfe8eaf6e1b44c5
Daniel Hwang <daniel@asta.lan>
2025-09-20 17:42:06 -0500

    Add git user email

--- newModules/home/core/programs/git.nix
+++ newModules/home/core/programs/git.nix
@@ -4,6 +4,7 @@
       git = {
         enable = true;
         userName = "Daniel Hwang";
+        userEmail = "daniel@asta.lan";
         extraConfig = {
           init.defaultBranch = "main";
         };
