c7dc3fc8e0e67587ddb8ed0cd72d0395ca9fe703
Daniel Hwang <daniel@asta.lan>
2025-09-23 01:50:50 -0500

    Add existing direnv module

---
+++ newModules/home/dev/direnv.nix
@@ -0,0 +1,10 @@
+{
+  flake.modules.homeManager.dev =
+  { pkgs, ... }:
+  {
+    programs.direnv = {
+      enable = true;
+      enableBashIntegration = true;
+      nix-direnv.enable = true;
+  };
+}
