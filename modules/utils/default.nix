4b1ea7376dc9d77bfd04d5b9e8936884544a1197
Daniel Hwang <dawh@ines.home>
2025-10-07 16:10:55 -0500

    Initial utils modules

---
+++ modules/utils/default.nix
@@ -0,0 +1,14 @@
+{ config, ... }:
+{
+  flake.modules = {
+    nixos.utils.imports = with config.flake.modules.nixos; [
+      undervolt
+    ];
+
+    homeManager.utils.imports = with config.flake.modules.homeManager; [
+      undervolt
+      gallery-dl
+      monitoring
+    ];
+  };
+}
