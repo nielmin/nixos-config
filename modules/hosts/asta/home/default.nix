c9aa3e0cf2f51857e3a5c2a48374a80389e70ed6
Daniel Hwang <dawh@ines.home>
2025-10-07 16:11:48 -0500

    Fix asta imports

--- modules/hosts/asta/home/default.nix
+++ modules/hosts/asta/home/default.nix
@@ -2,13 +2,11 @@
 {
   flake.modules.homeManager.host_asta = {
     imports = with config.flake.modules.homeManager; [
-      shell
-      desktop
       catppuccin
-      undervolt
-      direnv
-      tmux
-      gallery-dl
+      dev
+      desktop
+      utils
+      services
     ];
   };
 }
