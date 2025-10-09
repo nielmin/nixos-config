07756bc80097d3cc3e78c3891a5778c9b482d2ec
Daniel Hwang <daniel@asta.lan>
2025-10-08 20:04:23 -0500

    Add playerctl to niri nixos

--- modules/desktop/nixos/niri/default.nix
+++ modules/desktop/nixos/niri/default.nix
@@ -1,11 +1,19 @@
 { inputs, ... }:
 {
-  flake.modules.nixos.niri = {
-    nixpkgs.overlays = [
-      inputs.niri-flake.overlays.niri
-    ];
-    programs.niri = {
-      enable = true;
-    };    
-  };
+  flake.modules.nixos.niri =
+    { pkgs, ... }:
+    {
+      nixpkgs.overlays = [
+        inputs.niri-flake.overlays.niri
+      ];
+      programs.niri = {
+        enable = true;
+      };    
+      environment.systemPackages = with pkgs;
+      [
+        playerctl
+        wayland-utils # Wayland utilities
+        wl-clipboard # Command-line copy/paste utilities for Wayland
+      ];
+    };
 }
