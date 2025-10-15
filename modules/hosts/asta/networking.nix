d49df0ad8563d0190a2c6c8dc31f523b6bc9db70
Daniel Hwang <daniel@asta.lan>
2025-10-15 02:41:15 -0500

    Enable systemd-networkd

--- modules/hosts/asta/networking.nix
+++ modules/hosts/asta/networking.nix
@@ -2,9 +2,26 @@
   flake.modules.nixos.host_asta =
   { lib, ... }:
   {
+    systemd.network = {
+      enable = true;
+      networks = {
+        "20-wired" = {
+         matchConfig.Name = "en*";
+         networkConfig = {
+           DHCP = "yes";
+         };
+        };
+        "25-wireless" = {
+         matchConfig.Name = "wl*";
+         networkConfig = {
+           DHCP = "yes";
+         };
+        };
+      };
+    };
+
     networking = {
       hostName = "asta"; # Define your hostname.
-      useDHCP = false;
 
       networkmanager = {
         wifi.backend = "iwd";
