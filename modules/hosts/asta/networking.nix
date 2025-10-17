5361f8c04a6f902393a2b08f588d8a5cd4633e25
Daniel Hwang <daniel@asta.lan>
2025-10-17 18:39:48 -0500

    Update asta networking

--- modules/hosts/asta/networking.nix
+++ modules/hosts/asta/networking.nix
@@ -4,22 +4,27 @@
   {
     systemd.network = {
       enable = true;
-      networks = {
-        "20-wired" = {
-         matchConfig.Name = "en*";
-         networkConfig = {
-           DHCP = "yes";
-         };
-         linkConfig = {
-           RequiredForOnline = "no";
-           };
+      networks  = {
+        "40-enp0s31f6" = {
+          matchConfig.Name = "enp0s31f6";
+          networkConfig.DHCP = "yes";
+          linkConfig.RequiredForOnline = "no";
         };
 
-        "25-wireless" = {
-         matchConfig.Name = "wl*";
-         networkConfig = {
-           DHCP = "yes";
-         };
+        "40-wlp3s0" = {
+          matchConfig.name = "wlp3s0";
+          networkConfig.DHCP = "yes";
+          linkConfig.RequiredForOnline = "no";
+        };
+      };
+
+      links = {
+        "40-enp0s31f6" = {
+         matchConfig.Name = "enp0s31f6";
+        };
+
+        "40-wlp3s0" = {
+         matchConfig.name = "wlp3s0";
         };
       };
     };
@@ -29,21 +34,21 @@
       # useDHCP = lib.mkDefault true;
       dhcpcd.enable = false;
 
-      networkmanager = {
-        wifi.backend = "iwd";
-      };
+      # networkmanager = {
+      #   wifi.backend = "iwd";
+      # };
       
-      wireless.iwd = {
-        enable = true;
-        settings = {
-          General = {
-            EnableNetworkConfiguration = true;
-          };
-          Settings = {
-            AutoConnect = true;
-          };
-        };
-      };
+      # wireless.iwd = {
+      #   enable = true;
+      #   settings = {
+      #     General = {
+      #       EnableNetworkConfiguration = true;
+      #     };
+      #     Settings = {
+      #       AutoConnect = true;
+      #     };
+      #   };
+      # };
 
     };
   };
