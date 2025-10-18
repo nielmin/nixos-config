a0971c706cddd3695d066498fd9cbd4a45e80ff3
Daniel Hwang <daniel@asta.lan>
2025-10-18 02:26:13 -0500

    Update sound module

--- modules/desktop/sound.nix
+++ modules/desktop/sound.nix
@@ -1,6 +1,10 @@
 {
   flake.modules.nixos.sound = {
+    security.rtkit.enable = true;
+    
     services.pipewire = {
+      alsa.enable = true;
+      alsa.support32Bit = true;
       enable = true;
       pulse.enable = true;
     };
