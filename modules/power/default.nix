ce335ef7c64a24bf75f4529d9cff6804430857d5
Daniel Hwang <daniel@asta.lan>
2025-10-26 02:41:59 -0500

    Update tlp charge thresholds

--- modules/power/default.nix
+++ modules/power/default.nix
@@ -13,11 +13,11 @@
       tlp = {
         enable = true;
         settings = {
-          START_CHARGE_THRESH_BAT0=50
-          STOP_CHARGE_THRESH_BAT0=80
+          START_CHARGE_THRESH_BAT0=50;
+          STOP_CHARGE_THRESH_BAT0=80;
           
-          START_CHARGE_THRESH_BAT1=50
-          STOP_CHARGE_THRESH_BAT1=80
+          START_CHARGE_THRESH_BAT1=50;
+          STOP_CHARGE_THRESH_BAT1=80;
         };
       };
     };
