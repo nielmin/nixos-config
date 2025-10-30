2eab738ed141621df0e6360810ef9bea436e4256
Daniel Hwang <daniel@asta.lan>
2025-10-30 01:57:27 -0500

    Reformat tlp module

--- modules/power/default.nix
+++ modules/power/default.nix
@@ -1,25 +1,25 @@
 {
   flake.modules.nixos.power =
-  { pkgs, ... }:
-  {
-    environment.systemPackages = with pkgs; [
-      acpi
-      acpid
-    ];
+    { pkgs, ... }:
+    {
+      environment.systemPackages = with pkgs; [
+        acpi
+        acpid
+      ];
 
-    services = {
-      thermald.enable = true;
+      services = {
+        thermald.enable = true;
 
-      tlp = {
-        enable = true;
-        settings = {
-          START_CHARGE_THRESH_BAT0=50;
-          STOP_CHARGE_THRESH_BAT0=80;
-          
-          START_CHARGE_THRESH_BAT1=50;
-          STOP_CHARGE_THRESH_BAT1=80;
+        tlp = {
+          enable = true;
+          settings = {
+            START_CHARGE_THRESH_BAT0 = 50;
+            STOP_CHARGE_THRESH_BAT0 = 80;
+
+            START_CHARGE_THRESH_BAT1 = 50;
+            STOP_CHARGE_THRESH_BAT1 = 80;
+          };
         };
       };
     };
-  };
 }
