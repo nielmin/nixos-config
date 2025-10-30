3e303e190ce96b97cf1f9f02f2897a2c49adc056
Daniel Hwang <daniel@asta.lan>
2025-10-30 01:57:44 -0500

    Add quickshell flake

--- flake.nix
+++ flake.nix
@@ -62,6 +62,15 @@
     nixos-facter-modules = {
       url = "github:numtide/nixos-facter-modules";
     };
+
+    quickshell = {
+      # add ?ref=<tag> to track a tag
+      url = "git+https://git.outfoxxed.me/outfoxxed/quickshell";
+
+      # THIS IS IMPORTANT
+      # Mismatched system dependencies will lead to crashes and other issues.
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
   outputs =
