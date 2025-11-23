bb42e6ce4c2bfa0cdd311221745f0517c4577f43
Daniel Hwang <dawh@ines.home>
2025-11-23 17:22:20 -0600

    Import quadlet-nix to homelab

--- modules/homelab/default.nix
+++ modules/homelab/default.nix
@@ -1,6 +1,10 @@
 { config, ... }:
 {
   flake.modules = {
+    imports = [
+      quadlet-nix.nixosModules.quadlet
+    ];
+
     nixos.homelab.imports = with config.flake.modules.nixos; [
       jellyfin
     ];
