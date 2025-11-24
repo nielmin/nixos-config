766cfc27bb8021171d46edb64dc5c85ff5c20ed9
Daniel Hwang <dawh@ines.home>
2025-11-23 19:07:20 -0600

    Update radicale module

--- modules/homelab/radicale.nix
+++ modules/homelab/radicale.nix
@@ -3,7 +3,20 @@
   flake.modules.nixos.radicale = {
     services.radicale = {
       enable = true;
-      openFirewall = true;
+      settings = {
+        server = { 
+          hosts = [ "0.0.0.0:5232" ];
+        };
+        auth = {
+          type = "htpasswd";
+          htpasswd_filename = "/etc/radicale/users";
+          # hash function used for passwords. May be `plain` if you don't want to hash the passwords
+          htpasswd_encryption = "bcrypt";
+        };
+        storage = {
+          filesystem_folder = "/var/lib/radicale/collections";
+        };
+      };
     };
   };
 }
