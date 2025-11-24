05cc12f913846e2085784dc28e44676690a13a7c
Daniel Hwang <dawh@ines.home>
2025-11-23 18:58:27 -0600

    Set jellyfin user

--- modules/homelab/jellyfin.nix
+++ modules/homelab/jellyfin.nix
@@ -1,8 +1,10 @@
+{ config, ... }:
 {
   flake.modules.nixos.jellyfin = {
     services.jellyfin = {
       enable = true;
       openFirewall = true;
+      user = config.flake.meta.users.daniel.username;
     };
   };
 }
