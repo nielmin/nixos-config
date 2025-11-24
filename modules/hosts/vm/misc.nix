6e682b90474b2ddc774146d3d7f0666135aea173
Daniel Hwang <dawh@ines.home>
2025-11-23 18:04:35 -0600

    Move hostname declaration

--- modules/hosts/vm/misc.nix
+++ modules/hosts/vm/misc.nix
@@ -1,7 +1,5 @@
 {
   flake.modules.nixos.host_vm = {
-    networking.hostName = "vm";
-
     system.stateVersion = "24.11";
   };
 }
--- modules/hosts/vm/networking.nix
+++ modules/hosts/vm/networking.nix
@@ -1,6 +1,7 @@
 {
   flake.modules.nixos.host_vm = {
     networking = {
+      hostName = "vm";
       nftables.enable = true;
       firewall = {
         enable = false;
