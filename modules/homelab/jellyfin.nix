7018d4832797100548f368090d3fa25633f9f749
Daniel Hwang <dawh@ines.home>
2025-11-24 09:53:24 -0600

    Initial jellyfin quadlet

--- modules/homelab/jellyfin.nix
+++ modules/homelab/jellyfin.nix
@@ -1,10 +1,19 @@
 { config, ... }:
 {
-  flake.modules.nixos.jellyfin = {
-    services.jellyfin = {
-      enable = true;
-      openFirewall = true;
-      user = config.flake.meta.users.daniel.username;
+  flake.modules.homeManager.jellyfin = {
+    virtualisation.quadlet.containers = {
+      jellyfin = {
+        autoStart = true;
+        serviceConfig = {
+          RestartSec = "10";
+          Restart = "always";
+        };
+        containerConfig = {
+          image = "ghcr.io/jellyfin/jellyfin:latest";
+          publishPorts = [ "127.0.0.1:8096:8096" ];
+          userns = "keep-id";
+        };
+      };
     };
   };
 }
