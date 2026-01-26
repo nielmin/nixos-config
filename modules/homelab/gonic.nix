793f0b67e15dea2225f4e4bc28f9ef9bde007212
Daniel Hwang <dawh@ines.home>
2026-01-26 02:08:54 -0600

    Create gonic volume mounts

--- modules/homelab/gonic.nix
+++ modules/homelab/gonic.nix
@@ -1,5 +1,14 @@
 { config, ... }:
 {
+  flake.modules.nixos.gonic = {
+    systemd.tmpfiles.rules = [
+      "d /home/daniel/containers/gonic - daniel daniel - -"
+      "d /home/daniel/containers/gonic/data - daniel daniel - -"
+      "d /home/daniel/containers/gonic/playlists - daniel daniel - -"
+      "d /home/daniel/containers/gonic/podcasts - daniel daniel - -"
+    ];
+  };
+
   flake.modules.homeManager.gonic = {
     virtualisation.quadlet.containers = {
       gonic = {
