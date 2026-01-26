81caa3110cbbb0386f5c70cc13ab059d577dc4a7
Daniel Hwang <dawh@ines.home>
2026-01-25 18:56:46 -0600

    Initial vm preservation

---
+++ modules/hosts/vm/preservation.nix
@@ -0,0 +1,34 @@
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}:
+{
+  flake.modules.nixos.host_vm = {
+    preservation = {
+      enable = true;
+      preserveAt."/persistent" = {
+        files = [
+          # auto-generated machine ID
+          {
+            file = "/etc/machine-id";
+            inInitrd = true;
+          }
+        ];
+        directories = [
+          "/var/lib/systemd/timers"
+          # NixOS user state
+          "/var/lib/nixos"
+          "/var/log"
+        ];
+      };
+    };
+
+    # systemd-machine-id-commit.service would fail, but it is not relevant
+    # in this specific setup for a persistent machine-id so we disable it
+    #
+    # see the firstboot example below for an alternative approach
+    systemd.suppressedSystemUnits = [ "systemd-machine-id-commit.service" ];
+  };
+}
