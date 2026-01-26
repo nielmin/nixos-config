1404d09328f459345a2d3a35133eab2b8694b694
Daniel Hwang <dawh@ines.home>
2026-01-26 00:33:47 -0600

    Initial reset root service

--- modules/hosts/vm/hardware.nix
+++ modules/hosts/vm/hardware.nix
@@ -12,6 +12,47 @@
 
       boot = {
         initrd = {
+          systemd = {
+            enable = true;
+            services.rollback = {
+              description = "Rollback BTRFS root subvolume to a pristine state";
+              wantedBy = [ "initrd.target" ];
+              before = [ "sysroot.mount" ];
+
+              unitConfig = {
+                DefaultDependencies = "no";
+              };
+
+              serviceConfig = {
+                Type = "oneshot";
+              };
+
+              script = ''
+                mkdir /btrfs_tmp
+                mount /dev/disk/by-partlabel/disk-main-root /btrfs_tmp # CONFIRM THIS IS CORRECT FROM findmnt
+                if [[ -e /btrfs_tmp/root ]]; then
+                  mkdir -p /btrfs_tmp/old_roots
+                  timestamp=$(date --date="@$(stat -c %Y /btrfs_tmp/root)" "+%Y-%m-%-d_%H:%M:%S")
+                  mv /btrfs_tmp/root "/btrfs_tmp/old_roots/$timestamp"
+                fi
+
+                delete_subvolume_recursively() {
+                  IFS=$'\n'
+                  for i in $(btrfs subvolume list -o "$1" | cut -f 9- -d ' '); do
+                    delete_subvolume_recursively "/btrfs_tmp/$i"
+                  done
+                  btrfs subvolume delete "$1"
+                }
+
+                for i in $(find /btrfs_tmp/old_roots/ -maxdepth 1 -mtime +30); do
+                  delete_subvolume_recursively "$i"
+                done
+
+                btrfs subvolume create /btrfs_tmp/root
+                umount /btrfs_tmp
+              '';
+            };
+          };
           availableKernelModules = [
             "xhci_pci"
             "ahci"
@@ -22,7 +63,6 @@
             "virtio_blk"
           ];
           kernelModules = [ ];
-          systemd.enable = true;
         };
         kernelModules = [ "kvm-amd" ];
         extraModulePackages = [ ];
