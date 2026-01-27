b3d57a8ae2537209a9daf9009575df8e131d592b
Daniel Hwang <dawh@ines.home>
2026-01-26 20:40:27 -0600

    Update age secret path

--- modules/core/nixos/agenix.nix
+++ modules/core/nixos/agenix.nix
@@ -4,6 +4,6 @@
     imports = [
       inputs.agenix.nixosModules.default
     ];
-    age.secrets.vm_pass.file = ../../../secrets/vm_pass.age;
+    age.secrets.vm_pass.file = ../../secrets/vm_pass.age;
   };
 }
