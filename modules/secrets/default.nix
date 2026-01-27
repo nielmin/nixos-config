fe93d34e9aa5ee2d86bbdffd1a2a5117ce15e8a2
Daniel Hwang <dawh@ines.home>
2026-01-26 20:33:14 -0600

    Update secrets to a flake-part

---
+++ modules/secrets/default.nix
@@ -0,0 +1,15 @@
+{
+  flake.modules.nixos.secrets =
+    let
+      asta = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHKmr//V3GIOxwtWLy/KU8Qnj7BaGqtUJratYpKqkWvy daniel@asta";
+      ines = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIEr1KZ+SFRgEcIwCLWMp4bUnyJYtEgUSsR9nBHWR6/Vh daniel@ines";
+
+      users = [
+        asta
+        ines
+      ];
+    in
+    {
+      "vm_pass.age".publicKeys = users;
+    };
+}
