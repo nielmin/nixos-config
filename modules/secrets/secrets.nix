d9c9f5f08fcb46d5e67bacbe03e25139982ef6c5
Daniel Hwang <dawh@ines.home>
2026-01-26 19:39:09 -0600

    Move secrets to modules

--- modules/secrets/secrets.nix
+++ modules/secrets/secrets.nix
@@ -2,7 +2,10 @@ let
   asta = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHKmr//V3GIOxwtWLy/KU8Qnj7BaGqtUJratYpKqkWvy daniel@asta";
   ines = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIEr1KZ+SFRgEcIwCLWMp4bUnyJYtEgUSsR9nBHWR6/Vh daniel@ines";
 
-  users = [ asta ines ];
+  users = [
+    asta
+    ines
+  ];
 in
 {
   "vm_pass.age".publicKeys = users;
