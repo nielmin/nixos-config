34c6a5c1f3740f5edce1d5c71a22413b51dd2df1
Daniel Hwang <dawh@ines.home>
2026-01-31 20:04:58 -0600

    Add mjpg-streamer to octoprint

--- modules/services/octoprint.nix
+++ modules/services/octoprint.nix
@@ -1,11 +1,24 @@
 {
   flake.modules.nixos.octoprint = {
-    services.octoprint = {
-      enable = true;
-      user = "daniel";
-      group = "daniel";
-      openFirewall = true;
-      stateDir = "/home/daniel/octoprint";
+
+    networking.firewall.allowedTCPPorts = [ 5050 ];
+
+    services = {
+      mjpg-streamer = {
+        enable = true;
+        user = "mjpg-streamer";
+        group = "video";
+        inputPlugin = "input_uvc.so -d /dev/video0 -r 1280x720 -f 30";
+        outputPlugin = "output_http.so -w @www@ -n -p 5050";
+      };
+
+      octoprint = {
+        enable = true;
+        user = "daniel";
+        group = "daniel";
+        openFirewall = true;
+        stateDir = "/home/daniel/octoprint";
+      };
     };
   };
 }
