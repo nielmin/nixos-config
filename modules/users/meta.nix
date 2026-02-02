0573bf111e1eb3b575032f55a81ecbae60e93e94
Daniel Hwang <dawh@ines.home>
2026-02-01 19:05:37 -0600

    Remove all users except daniel

--- modules/users/meta.nix
+++ modules/users/meta.nix
@@ -9,20 +9,6 @@
           "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOo7qey0S5P0GgHBTGdZomCHq3zs6M43a/U5K/CwFGrq daniel@vm"
         ];
       };
-      nuc = {
-        name = "NUC";
-        username = "nuc";
-        authorizedKeys = [
-          "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIEr1KZ+SFRgEcIwCLWMp4bUnyJYtEgUSsR9nBHWR6/Vh daniel@ines"
-        ];
-      };
-      vm = {
-        name = "vm";
-        username = "daniel";
-        authorizedKeys = [
-          "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOo7qey0S5P0GgHBTGdZomCHq3zs6M43a/U5K/CwFGrq daniel@vm"
-        ];
-      };
     };
   };
 }
