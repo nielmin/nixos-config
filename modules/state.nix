144f1a361694122ab00db1c993f698670487e3d6
Daniel Hwang <dawh@ines.home>
2026-02-08 17:59:19 -0600

    Initial stateVersion module

---
+++ modules/state.nix
@@ -0,0 +1,14 @@
+{ self, ... }:
+let
+  stateVersion = "24.11";
+in
+{
+  flake.modules.nixos.state = {
+
+    system.stateVersion = "${stateVersion}";
+  };
+
+  flake.modules.homeManager.state = {
+    home.stateVersion = "${stateVersion}";
+  };
+}
