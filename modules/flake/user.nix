a31edc259dc7f40b477599fb93057121df6e9051
Daniel Hwang <dawh@ines.home>
2026-02-08 19:05:52 -0600

    Add daniel user initialPassword

--- modules/flake/user.nix
+++ modules/flake/user.nix
@@ -17,6 +17,8 @@
               "networkmanager"
             ];
             shell = pkgs.fish;
+            initialPassword = "changeme";
+
             openssh.authorizedKeys.keys = [
               "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIEr1KZ+SFRgEcIwCLWMp4bUnyJYtEgUSsR9nBHWR6/Vh daniel@ines"
               "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOo7qey0S5P0GgHBTGdZomCHq3zs6M43a/U5K/CwFGrq daniel@vm"
