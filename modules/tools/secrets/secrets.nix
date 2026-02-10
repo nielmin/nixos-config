0d4649cd2c5e8dcf82a29ffc0a36710a89343d3e
Daniel Hwang <dawh@ines.home>
2026-02-09 20:09:32 -0600

    Remove darwin.secrets

--- modules/tools/secrets/secrets.nix
+++ modules/tools/secrets/secrets.nix
@@ -13,15 +13,6 @@
       environment.systemPackages = [ inputs.agenix.packages.${pkgs.stdenv.hostPlatform.system}.default ];
     };
 
-  flake.modules.darwin.secrets =
-    { pkgs, ... }:
-    {
-      imports = [
-        inputs.agenix.darwinModules.default
-      ];
-      environment.systemPackages = [ inputs.agenix.packages.${pkgs.stdenv.hostPlatform.system}.default ];
-    };
-
   flake.modules.homeManager.secrets =
     { pkgs, ... }:
     {
