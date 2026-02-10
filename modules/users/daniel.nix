1ff2cc93498b4d2f3270fc70a3a1ed762646a04f
Daniel Hwang <dawh@ines.home>
2026-02-09 19:46:39 -0600

    Simplify daniel user

---
+++ modules/users/daniel.nix
@@ -0,0 +1,24 @@
+{
+  self,
+  lib,
+  ...
+}:
+let
+  username = "daniel";
+in
+{
+  flake.homeConfigurations = lib.mkHomeManager { username = "${username}"; };
+
+  flake.modules = lib.mkMerge [
+    (self.factory.user "${username}")
+    {
+      homeManager."${username}" = {
+        imports = with self.modules.homeManager; [
+          system-desktop
+          
+          alacritty
+        ];
+      };
+    }
+  ];
+}
