845a77ba69e7b37af5790258ac02755555002554
Daniel Hwang <daniel@asta.lan>
2026-03-31 17:43:37 -0500

    Enable home-manager for asta user

--- modules/hosts.nix
+++ modules/hosts.nix
@@ -1,6 +1,7 @@
 {
   den.hosts.x86_64-linux = {
     asta.users.daniel = { };
+    asta.users.daniel.classes = [ "homeManager" ];
     # host provides default home environment for its users
     # homeManager =
     #   { pkgs, ... }:
