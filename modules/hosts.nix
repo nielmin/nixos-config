393c778bfbe0462ba37b039d51fcaa40b0274ff3
Daniel Hwang <daniel@asta.lan>
2026-04-02 04:12:38 -0500

    Remove hm comments

--- modules/hosts.nix
+++ modules/hosts.nix
@@ -2,11 +2,5 @@
   den.hosts.x86_64-linux = {
     asta.users.daniel = { };
     asta.users.daniel.classes = [ "homeManager" ];
-    # host provides default home environment for its users
-    # homeManager =
-    #   { pkgs, ... }:
-    #   {
-    #     home.packages = [ pkgs.vim ];
-    #   };
   };
 }
