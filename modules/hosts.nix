036739ff5e4d19d071d92ad0b434961d5d9ab2bd
Daniel Hwang <daniel@asta.lan>
2026-04-02 14:13:56 -0500

    Add standalone home for daniel

--- modules/hosts.nix
+++ modules/hosts.nix
@@ -1,6 +1,7 @@
 {
-  den.hosts.x86_64-linux = {
-    asta.users.daniel = { };
-    asta.users.daniel.classes = [ "homeManager" ];
+  den.hosts.x86_64-linux.asta = {
+    users.daniel.classes = [ "homeManager" ];
+    home-manager.enable = true;
   };
+  den.homes.x86_64-linux.daniel = { };
 }
