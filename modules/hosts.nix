d2c50dd5ee18ab63454f538fecd60316cbb41f25
Daniel Hwang <daniel@asta.lan>
2026-04-02 14:42:24 -0500

    Update hosts.nix

--- modules/hosts.nix
+++ modules/hosts.nix
@@ -1,7 +1,7 @@
 {
   den.hosts.x86_64-linux.asta = {
-    users.daniel.classes = [ "homeManager" ];
-    home-manager.enable = true;
+    users.daniel = {
+      classes = [ "homeManager" ];
+    };
   };
-  den.homes.x86_64-linux.daniel = { };
 }
