c6c6f0d295041282626de994938bd99ec1cddf75
Daniel Hwang <dawh@ines.home>
2026-05-03 15:00:22 -0500

    Initial octoprint aspect

---
+++ modules/aspects/octoprint.nix
@@ -0,0 +1,15 @@
+{ den, ... }: {
+  den.aspects.services = {
+    nixos = { pkgs, ... }: {
+      services.undervolt = {
+        enable = true;
+        coreOffset = -70;
+      };
+    };
+    homeManager = { pkgs, ... }: {
+      services.syncthing = {
+        enable = true;
+      };
+    };
+  };
+}
