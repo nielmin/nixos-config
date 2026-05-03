a146dfbaea0571bd3c9083521992970fd8620201
Daniel Hwang <dawh@ines.home>
2026-05-03 17:01:16 -0500

    nix run .#write-flake

--- flake.nix
+++ flake.nix
@@ -4,7 +4,7 @@
   outputs = inputs: inputs.flake-parts.lib.mkFlake { inherit inputs; } (inputs.import-tree ./modules);
 
   inputs = {
-    den.url = "github:vic/den";
+    den.url = "github:denful/den";
     devshell.url = "github:numtide/devshell";
     flake-file.url = "github:vic/flake-file";
     flake-parts = {
