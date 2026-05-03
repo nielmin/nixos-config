423136cf71cb99a54ea12d611628d9369be2bd6f
Daniel Hwang <dawh@ines.home>
2026-05-03 15:05:13 -0500

    Update octoprint aspect

--- modules/aspects/octoprint.nix
+++ modules/aspects/octoprint.nix
@@ -1,15 +1,28 @@
 { den, ... }: {
   den.aspects.services = {
     nixos = { pkgs, ... }: {
-      services.undervolt = {
+    services = {
+      mjpg-streamer = {
         enable = true;
-        coreOffset = -70;
+        user = "mjpg-streamer";
+        group = "video";
+        inputPlugin = "input_uvc.so -d /dev/video0 -r 1280x720 -f 30";
+        outputPlugin = "output_http.so -w @www@ -n -p 5050";
+      };
+
+      octoprint = {
+        enable = true;
+        user = "daniel";
+        group = "daniel";
+        openFirewall = true;
+        stateDir = "/home/daniel/.config/octoprint";
+        plugins =
+          plugins: with plugins; [
+            themeify
+            stlviewer
+          ];
       };
     };
-    homeManager = { pkgs, ... }: {
-      services.syncthing = {
-        enable = true;
-      };
-    };
+  };
   };
 }
