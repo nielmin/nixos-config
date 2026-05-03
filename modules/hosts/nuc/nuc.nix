4bf0d850dd1f0dbe7f7221e3908b47492cfe1814
Daniel Hwang <dawh@ines.home>
2026-05-03 14:58:09 -0500

    Update: apply den to nuc host

---
+++ modules/hosts/nuc/nuc.nix
@@ -0,0 +1,56 @@
+{ den, ... }: {
+  den.aspects.asta = {
+    includes = [ 
+      den.aspects.bootable
+      den.aspects.kvm-intel
+      den.aspects.locale
+      den.aspects.networking
+      den.aspects.graphics
+      den.aspects.octoprint
+    ];
+    nixos = { pkgs, ... }: {
+
+      networking.hostName = "nuc";
+
+      hardware.facter.reportPath = ./facter.json;
+
+      boot.initrd.availableKernelModules = [ 
+        "xhci_pci"
+	      "ahci" 
+	      "usb_storage" 
+	      "sd_mod" 
+      ];
+      fileSystems."/" =
+    { device = "/dev/disk/by-uuid/7f5aa509-c734-4956-bc78-c6ad921be026";
+      fsType = "btrfs";
+      options = [ "subvol=rootfs" ];
+    };
+
+    fileSystems."/nix" =
+    { device = "/dev/disk/by-uuid/7f5aa509-c734-4956-bc78-c6ad921be026";
+      fsType = "btrfs";
+      options = [ "subvol=nix" ];
+    };
+
+    fileSystems."/home" =
+    { device = "/dev/disk/by-uuid/7f5aa509-c734-4956-bc78-c6ad921be026";
+      fsType = "btrfs";
+      options = [ "subvol=home" ];
+    };
+
+    fileSystems."/boot" =
+    { device = "/dev/disk/by-uuid/2A68-1504";
+      fsType = "vfat";
+      options = [ "fmask=0077" "dmask=0077" ];
+    };
+
+    fileSystems."/persist" =
+    { device = "/dev/disk/by-uuid/7f5aa509-c734-4956-bc78-c6ad921be026";
+      fsType = "btrfs";
+      options = [ "subvol=persist" ];
+    };
+
+      swapDevices = [ ];
+    };
+  };
+}
