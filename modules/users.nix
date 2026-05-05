7c2ef7e7900746e5d61c84640261574a826698da
Daniel Hwang <dawh@ines.home>
2026-05-04 23:07:48 -0500

    Enable sudo for nuc user

--- modules/users.nix
+++ modules/users.nix
@@ -30,6 +30,7 @@
     includes = [
       den.provides.define-user
       den.provides.primary-user
+      den.aspects.security
     ];
     user.hashedPassword = "$6$RkIPlT6IZxqyiuNG$u4ujjJEJe6kk7JHI.QaXSkwVYj8HBLKVm4Lr.I3DIHfyNsJqdWba.qajQRO.BPdq8e9fCoq58ROoexR/3F7hS.";
     nixos = {
