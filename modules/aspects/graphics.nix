330d4613e0ff76073d9bacf287a6fc2439f02242
Daniel Hwang <dawh@ines.home>
2026-05-04 23:33:49 -0500

    Add intel oneAPI to graphics aspect

--- modules/aspects/graphics.nix
+++ modules/aspects/graphics.nix
@@ -5,6 +5,7 @@
         enable = true;
         extraPackages = with pkgs; [
           intel-media-driver
+          vpl-gpu-rt
         ];
       };
       environment.sessionVariables = {LIBVA_DRIVER_NAME = "iHD";};
