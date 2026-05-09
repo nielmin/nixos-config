0535a4b7a2b4e97659d9cdb3c04fdca675c9d2a0
Daniel Hwang <dawh@ines.home>
2026-05-09 15:50:30 -0500

    Initial liv host

---
+++ modules/hosts/liv/system.nix
@@ -0,0 +1,26 @@
+{den, ...}: {
+  den.aspects.liv = {
+    includes = [
+      den.aspects.bootable
+      den.aspects.kvm-amd
+      den.aspects.gfx-amd
+      den.aspects.locale
+      den.aspects.networking
+      den.aspects.kde-desktop
+      den.aspects.services
+      den.aspects.dev
+    ];
+    nixos = {pkgs, ...}: {
+      networking.hostName = "liv";
+
+      hardware.facter.reportPath = ./facter.json;
+
+      boot.initrd.availableKernelModules = [
+        "xhci_pci"
+        "ahci"
+        "usb_storage"
+        "sd_mod"
+      ];
+    };
+  };
+}
