7df5b310a91cdbde871d8c068d1637ccd6a20902
Daniel Hwang <dawh@ines.home>
2026-05-11 14:57:26 -0500

    Update flakes

--- flake.nix
+++ flake.nix
@@ -4,7 +4,7 @@
   outputs = inputs: inputs.flake-parts.lib.mkFlake { inherit inputs; } (inputs.import-tree ./modules);
 
   inputs = {
-    den.url = "github:vic/den";
+    den.url = "github:denful/den";
     devshell.url = "github:numtide/devshell";
     disko = {
       url = "github:nix-community/disko";
