f271ab68122ac103399a9c756228dd800e5dffb2
Daniel Hwang <whadmia@gmail.com>
2026-05-17 01:48:40 -0500

    Disable mjpeg-streamer for octoprint

--- modules/aspects/octoprint.nix
+++ modules/aspects/octoprint.nix
@@ -1,18 +1,18 @@
 {den, ...}: {
   den.aspects.octoprint = {
     nixos = {pkgs, ...}: {
-      networking.firewall = {
-        allowedTCPPorts = [5050];
-      };
+      # networking.firewall = {
+      #   allowedTCPPorts = [5050];
+      # };
 
       services = {
-        mjpg-streamer = {
-          enable = true;
-          user = "mjpg-streamer";
-          group = "video";
-          inputPlugin = "input_uvc.so -d /dev/video0 -r 1920x1080 -f 30";
-          outputPlugin = "output_http.so -w @www@ -n -p 5050";
-        };
+        # mjpg-streamer = {
+        #   enable = true;
+        #   user = "mjpg-streamer";
+        #   group = "video";
+        #   inputPlugin = "input_uvc.so -d /dev/video0 -r 1920x1080 -f 30";
+        #   outputPlugin = "output_http.so -w @www@ -n -p 5050";
+        # };
 
         octoprint = {
           enable = true;
