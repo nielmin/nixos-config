fe550c6336644d4111e1f3bc7b01a09796d1323e
Daniel Hwang <whadmia@gmail.com>
2026-05-23 21:59:07 -0500

    Initial ines host config

--- modules/hosts/hosts.nix
+++ modules/hosts/hosts.nix
@@ -4,6 +4,10 @@
       users.daniel = { };
     };
 
+    ines = {
+      users.daniel = { };
+    };
+
     liv = {
       users.daniel = { };
     };
---
+++ modules/hosts/ines/system.nix
@@ -0,0 +1,30 @@
+{inputs, den, ...}: {
+  den.aspects.ines = {
+    includes = [
+      den.aspects.bootable
+      den.aspects.kvm-amd
+      den.aspects.gfx-amd
+      den.aspects.kde-desktop
+      den.aspects.services
+      den.aspects.dev
+    ];
+    nixos = {pkgs, ...}: {
+
+      imports = [
+        inputs.disko.nixosModules.disko
+        ./_disko.nix
+      ];
+
+      networking.hostName = "ines";
+
+      hardware.facter.reportPath = ./facter.json;
+
+      boot.initrd.availableKernelModules = [
+        "xhci_pci"
+        "ahci"
+        "usb_storage"
+        "sd_mod"
+      ];
+    };
+  };
+}
