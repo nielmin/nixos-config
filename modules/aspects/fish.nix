59faaef0becb0015509f8d8095ba453399d484b3
Daniel Hwang <daniel@ines.local>
2026-05-27 12:18:26 -0500

    Add rbw functions from existing dotfiles

--- modules/aspects/fish.nix
+++ modules/aspects/fish.nix
@@ -11,20 +11,14 @@
 	};
 	functions = {
 	  bwf = {
-	    argumentNames = [ "name" ];
-	    body = "
-	    set -f name $(rbw list | fzf --prompt='Vault > ')
-            [ -n '$name' ] && rbw get '$namei | wl-copy -n --sensitive
-            echo 'Copied Password: $name'
-	  ";
+	    body = "set -f name $(rbw list | fzf --prompt=\"Vault > \")
+            [ -n \"$name\" ] && rbw get \"$name\" | wl-copy -n --sensitive
+            echo \"Copied Password:\"$name\"\"";
 	  };
 	  bwft = {
-	    argumentNames = [ "totp" ];
-            body = "
-	      set -f totp $(rbw list | fzf --prompt='Vault (TOTP) > ')
-              [ -n '$totp' ] && rbw totp '$totp' | wl-copy -n --sensitive
-              echo 'Copied TOTP: $totp'
-	    ";
+            body = "set -f totp $(rbw list | fzf --prompt=\"Vault (TOTP) > \")
+              [ -n \"$totp\" ] && rbw totp \"$totp\" | wl-copy -n --sensitive
+              echo \"Copied TOTP: $totp\"";
 	  };
 	};
       };
