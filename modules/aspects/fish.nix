d32e3825a0ccc8f035a4522226c6ec0ad320b94f
Daniel Hwang <daniel@ines.local>
2026-05-27 13:49:50 -0500

    Add custom fish prompt

--- modules/aspects/fish.nix
+++ modules/aspects/fish.nix
@@ -9,7 +9,16 @@
           ls = "ls --color=auto --group-directories-first";
           ll = "ls -ahl --color=auto --group-directories-first";
         };
+        shellInit = "set -g fish_greeting";
         functions = {
+          fish_prompt = {
+            body = "set -l last_status $status
+            set -l stat
+            if test $last_status -ne 0
+                set stat (set_color red)\"[$last_status]\"(set_color normal)
+            end
+            string join '' -- $stat (set_color yellow) $hostname ' ' (set_color blue) (prompt_pwd --full-length-dirs 1) (set_color normal) ' '";
+          };
           bwf = {
             body = "set -f name $(rbw list | fzf --prompt=\"Vault > \")
             [ -n \"$name\" ] && rbw get \"$name\" | wl-copy -n --sensitive
