69b3eb682ca92cdffe38e85bc4b9ed55e83d83fd
Daniel Hwang <daniel@ines.local>
2026-05-30 14:02:00 -0500

    update

--- flake.nix
+++ flake.nix
@@ -16,7 +16,7 @@
       inputs.nixpkgs-lib.follows = "nixpkgs";
     };
     home-manager = {
-      url = "github:nix-community/home-manager";
+      url = "github:nix-community/home-manager/release-26.05";
       inputs.nixpkgs.follows = "nixpkgs";
     };
     import-tree.url = "github:vic/import-tree";
