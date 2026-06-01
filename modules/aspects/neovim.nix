035e3e1265a8be3a1036b6bd7e0ff2c79e4a6572
Daniel Hwang <daniel@ines.local>
2026-06-01 11:28:01 -0500

    Fix opts errors

--- modules/aspects/neovim.nix
+++ modules/aspects/neovim.nix
@@ -1,4 +1,8 @@
-{nlm, inputs, ...}: {
+{
+  nlm,
+  inputs,
+  ...
+}: {
   nlm.neovim = {
     homeManager = {pkgs, ...}: {
       imports = [
@@ -6,33 +10,32 @@
       ];
       programs.nixvim = {
         enable = true;
-				defaultEditor = true;
-    		opts = {
-					guicursor = "i:block";
-					colorcolumn = "80";
-					signcolumn = "yes:1";
-					termguicolors = true;
-					ignorecase = true; 
-					swapfile = false; 
-					autoindent = true; 
-					expandtab = true; 
-					tabstop = 2; 
-					softtabstop = 2;
-					shiftwidth = 2; 
-					shiftround = true;
-					list = true; 
-					number = true;
-					relativenumber = true;
-					numberwidth = 2; 
-					wrap = false; 
-					cursorline = true ;
-					scrolloff = 8; 
-					inccommand = "nosplit";
-					undofile = true; 
-					complete= [ "menuone" "popup" "noinsert"];
-					winborder = "rounded";
-					hlsearch = false; 
-    		};
+        defaultEditor = true;
+	withPython3 = false;
+	withRuby = false;
+        opts = {
+          colorcolumn = "80";
+          termguicolors = true;
+          ignorecase = true;
+          swapfile = false;
+          autoindent = true;
+          expandtab = true;
+          tabstop = 2;
+          softtabstop = 2;
+          shiftwidth = 2;
+          shiftround = true;
+          list = true;
+          number = true;
+          relativenumber = true;
+          numberwidth = 2;
+          wrap = false;
+          cursorline = true;
+          scrolloff = 8;
+          inccommand = "nosplit";
+          undofile = true;
+          completeopt = ["menuone" "popup" "noinsert"];
+          hlsearch = false;
+        };
       };
     };
   };
