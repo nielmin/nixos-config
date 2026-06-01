2b29e3faa6b570a97ec9a02e7476f1969dd7636b
Daniel Hwang <daniel@ines.local>
2026-06-01 11:18:57 -0500

    Add neovim opts

--- modules/aspects/neovim.nix
+++ modules/aspects/neovim.nix
@@ -6,6 +6,33 @@
       ];
       programs.nixvim = {
         enable = true;
+				defaultEditor = true;
+    		opts = {
+					guicursor = "i:block";
+					colorcolumn = "80";
+					signcolumn = "yes:1";
+					termguicolors = true;
+					ignorecase = true; 
+					swapfile = false; 
+					autoindent = true; 
+					expandtab = true; 
+					tabstop = 2; 
+					softtabstop = 2;
+					shiftwidth = 2; 
+					shiftround = true;
+					list = true; 
+					number = true;
+					relativenumber = true;
+					numberwidth = 2; 
+					wrap = false; 
+					cursorline = true ;
+					scrolloff = 8; 
+					inccommand = "nosplit";
+					undofile = true; 
+					complete= [ "menuone" "popup" "noinsert"];
+					winborder = "rounded";
+					hlsearch = false; 
+    		};
       };
     };
   };
