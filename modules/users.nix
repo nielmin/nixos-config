fe823f8e4613175dad34599da54bf86ea96fa397
Daniel Hwang <daniel@ines.local>
2026-06-01 18:13:13 -0500

    Update users group creation

--- modules/users.nix
+++ modules/users.nix
@@ -5,43 +5,48 @@
   __findFile,
   ...
 }: {
-  den.schema.user.classes = lib.mkDefault ["homeManager"];
+  den = {
+    schema.user.classes = lib.mkDefault ["homeManager"];
 
-  den.aspects = {
-    daniel = {
-      includes = [
-        <den.provides.define-user>
-        <den.provides.primary-user>
-        (den.provides.user-shell "fish")
-        <nlm/services>
-        <nlm/security>
-        <nlm/cli>
-        <nlm/multimedia>
-        <nlm/fish>
-        <nlm/dev>
-        <nlm/neovim>
-      ];
+    aspects = {
+      daniel = { host, user }: {
+        includes = [
+          <den.provides.define-user>
+          <den.provides.primary-user>
+          (den.provides.user-shell "fish")
+          <nlm/services>
+          <nlm/security>
+          <nlm/cli>
+          <nlm/multimedia>
+          <nlm/fish>
+          <nlm/dev>
+          <nlm/neovim>
+        ];
 
-      user.initialPassword = "changeme";
+        user.initialPassword = "changeme";
 
-      nixos = {
-        users.users.daniel.group = "daniel";
-        users.groups.daniel = {};
-      };
-    };
-    nuc = {
-      includes = [
-        <den.provides.define-user>
-        <den.provides.primary-user>
-        <nlm/security>
-      ];
-      user.hashedPassword = "$6$RkIPlT6IZxqyiuNG$u4ujjJEJe6kk7JHI.QaXSkwVYj8HBLKVm4Lr.I3DIHfyNsJqdWba.qajQRO.BPdq8e9fCoq58ROoexR/3F7hS.";
-      nixos = {
-        users.users.nuc = {
-          group = "nuc";
-          extraGroups = ["video"];
+        nixos = {
+          users.mutableUsers = true;
+          users.users."${user.userName}".group = "${user.userName}";
+          users.groups."${user.userName}" = {
+            gid = 1000;
+          };
         };
-        users.groups.nuc = {};
+      };
+      nuc = {
+        includes = [
+          <den.provides.define-user>
+          <den.provides.primary-user>
+          <nlm/security>
+        ];
+        user.hashedPassword = "$6$RkIPlT6IZxqyiuNG$u4ujjJEJe6kk7JHI.QaXSkwVYj8HBLKVm4Lr.I3DIHfyNsJqdWba.qajQRO.BPdq8e9fCoq58ROoexR/3F7hS.";
+        nixos = {
+          users.users.nuc = {
+            group = "nuc";
+            extraGroups = ["video"];
+          };
+          users.groups.nuc = {};
+        };
       };
     };
   };
