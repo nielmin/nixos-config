864e17745ca5147c6ff734ac5ef532ed0f6b42a2
Daniel Hwang <daniel@ines.local>
2026-06-04 17:06:23 -0500

    Enable nix-ld

--- modules/aspects/dev.nix
+++ modules/aspects/dev.nix
@@ -15,6 +15,17 @@
         enable = true;
         nix-direnv.enable = true;
       };
+
+      programs.nix-ld = {
+        enable = true;
+        libraries = with pkgs; [
+          stdenv.cc.cc.lib   # libstdc++.so.6 for most wheels
+          zlib               # CPython, many compiled extensions
+          openssl
+          libffi
+          glibc
+        ];
+      };
     };
 
     homeManager = {pkgs, ...}: {};
