e773a7b80e7883bc4d78d36f29ae4e9380cd237d
Daniel Hwang <daniel@ines.local>
2026-06-15 16:36:38 -0500

    Pin nixvim to nixpkgs

--- modules/flake/nixvim.nix
+++ modules/flake/nixvim.nix
@@ -3,6 +3,6 @@
     url = "github:nix-community/nixvim";
     # If you are not running an unstable channel of nixpkgs, select the corresponding branch of Nixvim.
     # url = "github:nix-community/nixvim/nixos-25.11";
-    # inputs.nixpkgs.follows = "nixpkgs";
+    inputs.nixpkgs.follows = "nixpkgs";
   };
 }
