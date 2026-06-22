6cddcc616f6a65aacfdda5e59ea51c884023d9a9
Daniel Hwang <daniel@ines.local>
2026-06-22 18:55:47 -0500

    Rewrite wrapped wezterm module

---
+++ modules/wrapped/wezterm/default.nix
@@ -0,0 +1,27 @@
+{
+  inputs,
+  lib,
+  ...
+}: {
+  nlm.wezterm = {
+    nixos = {
+      pkgs,
+      lib,
+      config,
+      ...
+    }: {
+      imports = [
+        inputs.nix-wrapper-modules.nixosModules.wezterm
+      ];
+      
+      wrappers.wezterm = {
+        enable = true;
+        package = pkgs.wezterm;
+
+        "wezterm.lua".content = builtins.readFile ./wezterm.lua;
+      };
+
+      environment.systemPackages = [ config.wrappers.wezterm.package ];
+    };
+  };
+}
