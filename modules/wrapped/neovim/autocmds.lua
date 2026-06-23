e34408828df853878dca559cdfa0befd4e0e6084
Daniel Hwang <daniel@ines.local>
2026-06-22 19:55:19 -0500

    Update lsp.lua to dotfiles autocmds.lua

---
+++ modules/wrapped/neovim/autocmds.lua
@@ -0,0 +1,27 @@
+local autocmd = vim.api.nvim_create_autocmd
+local augroup = vim.api.nvim_create_augroup
+
+autocmd("TextYankPost", {
+  desc = "Highlight when yanking (copying) text",
+  group = augroup("YankHighlight", { clear = true }),
+  callback = function()
+    vim.highlight.on_yank({ timeout = 170 })
+  end,
+})
+
+autocmd('LspAttach', {
+  group = augroup("lsp-attach", { clear = true }),
+  callback = function(event)
+    local map = function(keys, func, desc)
+      vim.keymap.set("n", keys, func, { buffer = event.buf, desc = "LSP: " .. desc })
+    end
+
+    map('K', vim.lsp.buf.hover, "Hover Documentation")
+    map('gd', vim.lsp.buf.definition, "Goto Definition")
+    map('gD', vim.lsp.buf.declaration, "Goto Declaration")
+    map('gi', vim.lsp.buf.implementation, "Goto Implementation")
+    map('gr', vim.lsp.buf.references, "Goto all references")
+    map('<leader>rn', vim.lsp.buf.rename, "Rename all references")
+    map('<leader>ca', vim.lsp.buf.code_action, "Code Action")
+  end,
+})
