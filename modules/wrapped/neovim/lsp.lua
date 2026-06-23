714398a79b5f9e13916a9665ee1547524bf58e6f
Daniel Hwang <daniel@ines.local>
2026-06-22 20:57:32 -0500

    Consolidate lsp configs into lsp.lua

--- modules/wrapped/neovim/lsp.lua
+++ modules/wrapped/neovim/lsp.lua
@@ -1,3 +1,48 @@
+if vim.lsp.config then
+  vim.lsp.config("gopls", {
+    cmd = { "gopls" },
+    filetypes = { "go", "gomod", "gowork", "gotmpl", "gosum" },
+    root_markers = { "go.mod", "go.work", ".git" },
+    settings = {
+      gopls = {
+        gofumpt = true,
+        staticcheck = true,
+        analyses = {
+          nilness = true,
+          unusedparams = true,
+        },
+      },
+    },
+  })
+
+  vim.lsp.config("lua_ls", {
+    cmd = { "lua-language-server" },
+    filetypes = { "lua" },
+    root_markers = { "init.lua", ".git" },
+    settings = {
+      Lua = {
+        runtime = {
+          version = "LuaJIT",
+        },
+        diagnostics = {
+          -- Get the language server to recognize the `vim` global variable
+          globals = { "vim" },
+        },
+        workspace = {
+          -- Make the server aware of Neovim runtime files
+          library = vim.api.nvim_get_runtime_file("", true),
+          checkThirdParty = false,
+        },
+        telemetry = {
+          enable = false,
+        },
+      },
+    },
+  })
+end
+
+-- 2. Activate the language server
 vim.lsp.enable({
   "gopls",
+  "lua_ls",
 })
