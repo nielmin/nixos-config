8ccb4e2b1482454a32e3145e1fba174f9084edd8
Daniel Hwang <daniel@ines.local>
2026-06-29 20:13:21 -0500

    Initial samba secret

--- modules/flake/sops.nix
+++ modules/flake/sops.nix
@@ -10,10 +10,27 @@
       imports = [
         inputs.sops-nix.nixosModules.sops
       ];
+      
+      sops = {
+        defaultSopsFile = ../../.sops.yaml;
+        age.sshKeyPaths = [ "/etc/ssh/ssh_host_ed25519_key" ];
+
+        secrets = {
+          "samba" = {
+            sopsFile = ../../secrets/samba.yaml;
+            key = "samba";
+            owner = "root";
+            group = "root";
+            mode = "0400";
+          };
+        };
+      };
 
       environment.systemPackages = with pkgs; [
         sops
         age
+        ssh-to-age
+        ssh-to-pgp
       ];
     };
   };
