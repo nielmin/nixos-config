b7359bbef94aa127c8f04d69385659e882cfb72d
Daniel H <daniel@liv.local>
2026-07-04 06:05:45 -0500

    noctalia-greeter: init flake-file

--- modules/flake/noctalia-greeter.nix
+++ modules/flake/noctalia-greeter.nix
@@ -1,23 +1,6 @@
 {
-  inputs,
-  nlm,
-  ...
-}: {
   flake-file.inputs.noctalia-greeter = {
     url = "github:noctalia-dev/noctalia-greeter";
     inputs.nixpkgs.follows = "nixpkgs";
-  };
-
-  nlm.noctalia-greeter = {
-    nixos = {pkgs, ...}: {
-      imports = [
-        inputs.noctalia-greeter.nixosModules.default
-      ];
-
-      programs.noctalia-greeter = {
-        enable = true;
-      };
-
-    };
   };
 }
