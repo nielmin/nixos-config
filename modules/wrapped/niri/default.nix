88055c67cd35db5ca8956fe15a4df13cdce3ec9d
Daniel H <daniel@liv.local>
2026-07-04 01:24:17 -0500

    Initial wrapped niri config

---
+++ modules/wrapped/niri/default.nix
@@ -0,0 +1,21 @@
+{
+  nlm,
+    inputs,
+    ...
+}: {
+  nlm.niri = {
+    nixos = { pkgs, lib, config, ...}: {
+      imports = [
+        inputs.nix-wrapper-modules.nixosModules.niri
+      ];
+
+      wrappers.niri = {
+        enable = true;
+        "config.kdl".path = ./config.kdl;
+      };
+      environment.systemPackages = [ config.wrappers.niri.package ];
+    };
+
+    homeManager = {pkgs, ...}: {};
+  };
+}
