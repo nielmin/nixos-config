64a60eae21084ebecc58e595e103e77f91a281e0
Daniel H <whadmia@gmail.com>
2026-07-06 23:01:32 -0500

    fuzzel: update fuzzel config

--- modules/wrapped/fuzzel/default.nix
+++ modules/wrapped/fuzzel/default.nix
@@ -19,7 +19,7 @@
             font = "Agave Nerd Font:weight=bold:size=10";
             dpi-aware = "no";
             terminal = "wezterm";
-            prompt = ">";
+            prompt = "❯ ";
             icon-theme = "Adwaita";
             width = 35;
             horizontal-pad = 10;
@@ -29,9 +29,20 @@
             letter-spacing = 0;
             anchor = "center";
             layer = "overlay";
-            exit-on-keyboard-focuf-loss = "yes";
+            exit-on-keyboard-focus-loss = "yes";
           };
-
+          colors = {
+            background = "0a0a0fe6";
+            text = "f8f8f2ff";
+            match = "8be9fdff";
+            selection = "8be9fdff";
+            selection-text = "0a0a0fff";
+            selection-match = "0a0a0fff";
+          };
+          dmenu = {
+            mode = "text";
+            exit-immediately-if-empty = "no";
+          };
         };
       };
     };
