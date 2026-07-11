ff9a9a8acea5649f7bf332be1b434696efd9f0e5
Daniel H <whadmia@gmail.com>
2026-07-11 16:25:14 -0500

    services: initial kanata config

--- modules/aspects/services.nix
+++ modules/aspects/services.nix
@@ -20,6 +20,20 @@
         syncthing = {
           enable = true;
         };
+
+        kanata = {
+          enable = true;
+          keyboards.default = {
+            config = ''
+            (defsrc
+              caps)
+
+            (deflayermap (default-layer)
+              ;; tap caps lock as caps lock, hold caps lock as left control
+              caps (tap-hold 100 100 caps lctl))
+            '';
+          };
+        };
       };
     };
   };
