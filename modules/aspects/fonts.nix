ae445deaf0934bab089a6ee7e98e881a0afc7067
Daniel Hwang <whadmia@gmail.com>
2026-07-24 12:17:55 -0500

    fonts: add inter and atkinson-hyperlegible

--- modules/aspects/fonts.nix
+++ modules/aspects/fonts.nix
@@ -2,11 +2,17 @@
   nlm.fonts = {
     nixos = {pkgs, ...}: {
       fonts.packages = with pkgs; [
+        agave
+        nerd-fonts.agave
+
+        atkinson-hyperlegible-next
+        atkinson-hyperlegible-mono
+
+        inter
+
         noto-fonts-cjk-sans
         noto-fonts-cjk-serif
 
-        agave
-        nerd-fonts.agave
       ];
     };
 
