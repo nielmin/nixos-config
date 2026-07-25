35fb764a79e7946240af3d0a479dba53f3e76fb2
Daniel Hwang <whadmia@gmail.com>
2026-07-25 14:31:00 -0500

    virtualisation: enable incus webui

--- modules/aspects/virtualisation.nix
+++ modules/aspects/virtualisation.nix
@@ -1,7 +1,12 @@
 {nlm, ...}: {
   nlm.virtualisation = {
     nixos = {pkgs, ...}: {
-      virtualisation.incus.enable = true;
+
+      virtualisation.incus = {
+        enable = true;
+        ui.enable = true;
+      };
+
       networking.firewall.trustedInterfaces = ["incusbr0"];
     };
   };
