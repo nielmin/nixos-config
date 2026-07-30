5955d41b91a53eb4183e860eabba7af41b0b8ba2
Daniel Hwang <whadmia@gmail.com>
2026-07-30 17:13:13 -0500

    neovim: fix rust-analyzer lsp config

--- modules/wrapped/neovim/config/lua/lsp.lua
+++ modules/wrapped/neovim/config/lua/lsp.lua
@@ -66,9 +66,25 @@ if vim.lsp.config then
   })
 
   vim.lsp.config("rust_analyzer", {
-    filetypes = { "rs" },
+    cmd = { "rust-analyzer" },
+    filetypes = { "rust" },
+    root_markers = { "Cargo.toml", "rust-project.json", ".git" },
     settings = {
       ["rust-analyzer"] = {
+        imports = {
+          granularity = {
+            group = "module",
+          },
+          prefix = "self",
+        },
+        cargo = {
+          buildScripts = {
+            enable = true,
+          },
+        },
+        procMacro = {
+          enable = true,
+        },
         diagnostics = {
           enable = true,
         },
