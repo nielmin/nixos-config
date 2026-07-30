3af8d64a8bb2ecc3d3008e219b543688d2f3c43b
Daniel Hwang <whadmia@gmail.com>
2026-07-29 21:56:19 -0500

    gfx-amd: remove lact override

--- modules/aspects/gfx-amd.nix
+++ modules/aspects/gfx-amd.nix
@@ -10,20 +10,7 @@
       };
       services.lact = {
         enable = true;
-        package = pkgs.lact.override {
-           libdisplay-info = pkgs.libdisplay-info.overrideAttrs (finalAttrs: {
-              version = "0.3.0";
-              src = pkgs.fetchFromGitLab {
-                domain = "gitlab.freedesktop.org";
-                owner = "emersion";
-                repo = "libdisplay-info";
-                rev = finalAttrs.version;
-                sha256 = "sha256-nXf2KGovNKvcchlHlzKBkAOeySMJXgxMpbi5z9gLrdc=";
-            };
-         });
-        };
       };
     };
   };
 }
-
