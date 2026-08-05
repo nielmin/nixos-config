6c73fe93307cb8dd05acaf68aae5cbcf4f0e8b34
Daniel H <whadmia@gmail.com>
2026-08-05 13:55:55 -0500

    stylix: set scheme and cursor

--- modules/aspects/stylix.nix
+++ modules/aspects/stylix.nix
@@ -4,23 +4,36 @@
   ...
 }: {
   nlm.stylix = {
-    nixos = {
-      config,
-      pkgs,
-      lib,
-      ...
-    }: {
+    nixos = {pkgs, ...}: {
       imports = [
         inputs.stylix.nixosModules.stylix
       ];
 
-      stylix.enable = true;
+      stylix = {
+        enable = true;
+
+        base16Scheme = "${pkgs.base16-schemes}/share/themes/tokyo-night-dark.yaml";
+
+        cursor = {
+          package = pkgs.bibata-cursors;
+          name = "Bibata-Modern-Amber";
+          size = 32;
+        };
+
+        targets = {
+          gnome.enable = false;
+        };
+      };
     };
 
     homeManager = {pkgs, ...}: {
       imports = [
         inputs.stylix.homeModules.stylix
       ];
+
+      stylix = {
+        gnome.enable = false;
+      };
     };
   };
 }
