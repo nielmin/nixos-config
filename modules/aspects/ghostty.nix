0e80426f58be31b266feb2443fa47fc19b565c11
Daniel H <whadmia@gmail.com>
2026-08-06 15:54:17 -0500

    ghostty: set window-decoration to none

--- modules/aspects/ghostty.nix
+++ modules/aspects/ghostty.nix
@@ -9,6 +9,8 @@
     homeManager = {...}: {
       xdg.configFile = {
         "ghostty/config.ghostty".text = ''
+          window-decoration = none
+
           theme = dark:koda-dark,light:koda-light
 
           font-family = IoskeleyMonoTerm Nerd Font
