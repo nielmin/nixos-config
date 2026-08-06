9a5d8afc89e743f0bd2aab7148cd02f8f3ed4ae8
Daniel H <whadmia@gmail.com>
2026-08-06 15:49:54 -0500

    stylix: GEMINI add koda-nvim scheme

--- modules/aspects/stylix.nix
+++ modules/aspects/stylix.nix
@@ -14,7 +14,27 @@
       stylix = {
         enable = true;
 
-        base16Scheme = "${pkgs.base16-schemes}/share/themes/tokyo-night-dark.yaml";
+        base16Scheme = {
+          scheme = "koda";
+          polarity = "dark";
+
+          base00 = "101010"; # Default Background
+          base01 = "272727"; # Lighter Background (Selection Background)
+          base02 = "383838"; # Selection Highlight
+          base03 = "50585d"; # Comments, Invisibles, Dimmed Text (Palette 8)
+          base04 = "a5adce"; # Dark Foreground / Status Bar Text (Palette 7)
+          base05 = "ffffff"; # Default Foreground / Caret
+          base06 = "fafafa"; # Light Foreground (Palette 6)
+          base07 = "b5bfe2"; # Light Background / Bright White (Palette 15)
+          base08 = "ff7676"; # Red / Variables (Palette 1)
+          base09 = "ff5733"; # Orange / Integers, Constants (Palette 9)
+          base0A = "d9ba73"; # Yellow / Classes, Types (Palette 11)
+          base0B = "a3d6a3"; # Green / Strings (Palette 2)
+          base0C = "5abfb5"; # Cyan / Support, Regex (Palette 14)
+          base0D = "b3b3b3"; # Blue / Functions, Methods (Palette 4)
+          base0E = "f4b8e4"; # Magenta / Keywords (Palette 5)
+          base0F = "f2a4db"; # Pink / Extra Accent (Palette 13)
+        };
 
         cursor = {
           package = pkgs.bibata-cursors;
